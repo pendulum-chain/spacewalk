8d0099714a66ad65d142c81dbcb1c8988c624fcc
Marcel Ebert <mail@marcel-ebert.de>
2022-03-30 18:03:08 +0200

    Add handling for redeem requests for horizon

--- clients/vault/src/error.rs
+++ clients/vault/src/error.rs
@@ -4,6 +4,9 @@ use jsonrpc_core_client::RpcError;
 use parity_scale_codec::Error as CodecError;
 use runtime::{Error as RuntimeError, SubxtError};
 use service::Error as ServiceError;
+use sp_runtime::traits::LookupError;
+use sp_std::str::Utf8Error;
+use substrate_stellar_sdk::StellarSdkError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -42,4 +45,31 @@ pub enum Error {
 
     #[error("Error returned when fetching remote info")]
     HttpFetchingError,
+    #[error("Lookup Error")]
+    LookupError,
+    #[error("Stellar SDK Error")]
+    StellarSdkError,
+    #[error("ExceedsMaximumLengthError")]
+    ExceedsMaximumLengthError,
+    #[error("Failed to convert balance")]
+    BalanceConversionError,
+    #[error("Utf8Error: {0}")]
+    Utf8Error(#[from] Utf8Error),
+    #[error("Failed to parse sequence number")]
+    SeqNoParsingError,
+}
+
+impl From<StellarSdkError> for Error {
+    fn from(error: StellarSdkError) -> Self {
+        match error {
+            StellarSdkError::ExceedsMaximumLength { .. } => Self::ExceedsMaximumLengthError,
+            _ => Self::StellarSdkError,
+        }
+    }
+}
+
+impl From<LookupError> for Error {
+    fn from(_: LookupError) -> Self {
+        Self::BalanceConversionError
+    }
 }
--- clients/vault/src/horizon.rs
+++ clients/vault/src/horizon.rs
@@ -1,8 +1,8 @@
-use crate::error::Error;
+use crate::{error::Error, execution::*, system::VaultIdManager};
 use parity_scale_codec::{Decode, Encode, MaxEncodedLen};
-use runtime::{AccountId, Balance, InterBtcParachain, SpacewalkPallet};
+use runtime::{AccountId, Balance, InterBtcParachain, RedeemPallet, RequestRedeemEvent, SpacewalkPallet};
 use serde::{Deserialize, Deserializer};
-use service::Error as ServiceError;
+use service::{spawn_cancelable, Error as ServiceError, ShutdownSender};
 use sp_core::ed25519;
 use sp_runtime::{
     scale_info::TypeInfo,
@@ -417,13 +417,196 @@ async fn fetch_horizon_and_process_new_transactions(parachain_rpc: &InterBtcPara
     }
 }
 
-const POLL_INTERVAL_SECONDS: u64 = 5;
+const POLL_INTERVAL: u64 = 5000;
+const FETCH_TIMEOUT_PERIOD: u64 = 3000;
+const SUBMISSION_TIMEOUT_PERIOD: u64 = 10000;
 
 pub async fn poll_horizon_for_new_transactions(parachain_rpc: InterBtcParachain) -> Result<(), ServiceError> {
     // Start polling horizon every 5 seconds
     loop {
         fetch_horizon_and_process_new_transactions(&parachain_rpc).await;
 
-        sleep(Duration::from_secs(POLL_INTERVAL_SECONDS)).await;
+        sleep(Duration::from_millis(POLL_INTERVAL)).await;
     }
+}
+
+// Withdrawal
+
+async fn fetch_from_remote(request_url: &str) -> Result<HorizonAccountResponse, Error> {
+    tracing::info!("Sending request to: {}", request_url);
+
+    let response = reqwest::get(request_url)
+        .await
+        .map_err(|e| Error::HttpFetchingError)?
+        .json::<HorizonAccountResponse>()
+        .await
+        .map_err(|e| Error::HttpFetchingError)?;
+
+    Ok(response)
+}
+
+async fn fetch_latest_seq_no(stellar_addr: &str) -> Result<u64, Error> {
+    let request_url = String::from("https://horizon-testnet.stellar.org/accounts/") + stellar_addr;
+
+    let horizon_response = fetch_from_remote(request_url.as_str()).await.map_err(|e| {
+        tracing::error!("fetch_latest_seq_no error: {:?}", e);
+        Error::HttpFetchingError
+    })?;
+
+    String::from_utf8(horizon_response.sequence)
+        .map(|string| string.parse::<u64>().unwrap())
+        .map_err(|_| Error::SeqNoParsingError)
+}
+
+fn sign_stellar_tx(
+    tx: stellar::types::Transaction,
+    secret_key: SecretKey,
+) -> Result<stellar::TransactionEnvelope, Error> {
+    let mut envelope = tx.into_transaction_envelope();
+    envelope.sign(&TEST_NETWORK, vec![&secret_key])?;
+
+    Ok(envelope)
+}
+
+fn submit_stellar_tx(tx: stellar::TransactionEnvelope) -> Result<(), Error> {
+    let mut last_error: Option<Error> = None;
+
+    for attempt in 1..=3 {
+        tracing::debug!("Attempt #{} to submit Stellar transaction…", attempt);
+
+        match try_once_submit_stellar_tx(&tx) {
+            Ok(result) => {
+                return Ok(result);
+            }
+            Err(error) => {
+                last_error = Some(error);
+            }
+        }
+    }
+
+    // Can only panic if no submission was ever attempted
+    Err(last_error.unwrap())
+}
+
+fn try_once_submit_stellar_tx(tx: &stellar::TransactionEnvelope) -> Result<(), Error> {
+    let horizon_base_url = "https://horizon-testnet.stellar.org";
+    let horizon = stellar::horizon::Horizon::new(horizon_base_url);
+
+    tracing::info!("Submitting transaction to Stellar network: {}", horizon_base_url);
+
+    let _response = horizon
+        .submit_transaction(&tx, SUBMISSION_TIMEOUT_PERIOD, true)
+        .map_err(|error| {
+            match error {
+                stellar::horizon::FetchError::UnexpectedResponseStatus { status, body } => {
+                    tracing::error!("Unexpected HTTP request status code: {}", status);
+                    tracing::error!("  Response body: {}", str::from_utf8(&body).unwrap());
+                }
+                _ => (),
+            }
+            Error::HttpFetchingError
+        })?;
+
+    Ok(())
+}
+
+fn create_withdrawal_tx(
+    stellar_addr: &stellar::PublicKey,
+    seq_num: i64,
+    asset: stellar::Asset,
+    amount: Balance,
+) -> Result<stellar::Transaction, Error> {
+    let destination_addr = stellar_addr.as_binary();
+
+    let source_keypair: SecretKey = SecretKey::from_encoding(ESCROW_SECRET_KEY).unwrap();
+
+    let source_pubkey = source_keypair.get_public().clone();
+
+    let mut tx = stellar::Transaction::new(source_pubkey, seq_num, Some(10_000), None, None)?;
+
+    tx.append_operation(stellar::Operation::new_payment(
+        stellar::MuxedAccount::KeyTypeEd25519(*destination_addr),
+        asset,
+        stellar::StroopAmount(BalanceConversion::lookup(amount).map_err(|_| Error::BalanceConversionError)?),
+    )?)?;
+
+    Ok(tx)
+}
+
+async fn execute_withdrawal(amount: Balance, currency_id: CurrencyId, destination: AccountId) -> Result<(), Error> {
+    let pendulum_account_id = destination;
+
+    let asset = CurrencyConversion::lookup(currency_id)?;
+
+    let escrow_keypair: SecretKey = SecretKey::from_encoding(ESCROW_SECRET_KEY).unwrap();
+    let escrow_encoded = escrow_keypair.get_public().to_encoding().clone();
+    let escrow_address = str::from_utf8(escrow_encoded.as_slice())?;
+
+    let stellar_address = AddressConversion::lookup(pendulum_account_id.clone())?;
+
+    tracing::info!("Execute withdrawal: ({:?}, {:?})", currency_id, amount,);
+
+    let seq_no = fetch_latest_seq_no(escrow_address).await.map(|seq_no| seq_no + 1)?;
+    let transaction = create_withdrawal_tx(&stellar_address, seq_no as i64, asset, amount)?;
+    let signed_envelope = sign_stellar_tx(transaction, escrow_keypair)?;
+
+    let result = submit_stellar_tx(signed_envelope);
+    tracing::info!(
+        "✔️  Successfully submitted withdrawal transaction to Stellar, crediting {}",
+        str::from_utf8(stellar_address.to_encoding().as_slice()).unwrap()
+    );
+
+    result
+}
+
+/// Listen for RequestRedeemEvent directed at this vault; upon reception, transfer
+/// bitcoin and call execute_redeem
+///
+/// # Arguments
+///
+/// * `parachain_rpc` - the parachain RPC handle
+pub async fn listen_for_redeem_requests(
+    shutdown_tx: ShutdownSender,
+    parachain_rpc: InterBtcParachain,
+) -> Result<(), ServiceError> {
+    tracing::info!("Starting to listen for redeem requests…");
+    parachain_rpc
+        .on_event::<RequestRedeemEvent, _, _, _>(
+            |event| async {
+                tracing::info!("Received redeem request: {:?}", event);
+
+                // within this event callback, we captured the arguments of listen_for_redeem_requests
+                // by reference. Since spawn requires static lifetimes, we will need to capture the
+                // arguments by value rather than by reference, so clone these:
+                // Spawn a new task so that we handle these events concurrently
+                spawn_cancelable(shutdown_tx.subscribe(), async move {
+                    tracing::info!("Executing redeem #{:?}", event.redeem_id);
+                    // FIXME
+                    let currency_id: CurrencyId = CurrencyId::Native;
+                    let destination = AccountId::new([1u8; 32]);
+                    let amount = 1000;
+
+                    // let destination = AccountId::from(event.destination_account_id);
+                    // let currency_id = event.currenncy_id;
+                    // let amount = event.amount;
+                    let result = execute_withdrawal(amount, currency_id, destination).await;
+
+                    match result {
+                        Ok(_) => tracing::info!(
+                            "Completed redeem request #{} with amount {}",
+                            event.redeem_id,
+                            event.amount
+                        ),
+                        Err(e) => tracing::error!(
+                            "Failed to process redeem request #{}: {}",
+                            event.redeem_id,
+                            e.to_string()
+                        ),
+                    }
+                });
+            },
+            |error| tracing::error!("Error reading redeem event: {}", error.to_string()),
+        )
+        .await?;
+    Ok(())
 }
