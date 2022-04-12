37615127575be585218396a2716917cc514446ec
Hugo Lindsay <hugo@satoshipay.io>
2022-04-12 12:33:18 -0400

    remove vaults folder before pull request

--- clients/runtime/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    // Tell Cargo that if the given file changes, to rerun this build script (i.e. recompile)
-    println!("cargo:rerun-if-changed=metadata-parachain-kintsugi.scale");
-    println!("cargo:rerun-if-changed=metadata-parachain-testnet.scale");
-    println!("cargo:rerun-if-changed=metadata-standalone.scale");
-}
