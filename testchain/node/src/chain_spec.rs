bdfd43a80e85a2b3d50a5db7094ace826502bb72
Marcel Ebert <mail@marcel-ebert.de>
2022-04-20 18:02:42 +0200

    Add sr25519 Charlie and Bob to endowed accounts

--- testchain/node/src/chain_spec.rs
+++ testchain/node/src/chain_spec.rs
@@ -60,7 +60,9 @@ pub fn local_config() -> ChainSpec {
 				vec![
 					get_account_id_from_seed::<sr25519::Public>("Alice"),
 					get_account_id_from_seed::<ed25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
 					get_account_id_from_seed::<ed25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie"),
 					get_account_id_from_seed::<ed25519::Public>("Charlie"),
 					get_account_id_from_seed::<ed25519::Public>("Dave"),
 					get_account_id_from_seed::<ed25519::Public>("Eve"),
@@ -166,7 +168,9 @@ pub fn development_config() -> ChainSpec {
 				vec![
 					get_account_id_from_seed::<sr25519::Public>("Alice"),
 					get_account_id_from_seed::<ed25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
 					get_account_id_from_seed::<ed25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie"),
 					get_account_id_from_seed::<ed25519::Public>("Charlie"),
 					get_account_id_from_seed::<ed25519::Public>("Dave"),
 					get_account_id_from_seed::<ed25519::Public>("Eve"),
