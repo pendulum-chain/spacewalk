1f679b8861705487ee79b71ec336a8a6e572ad76
Hugo Lindsay <8341280+ashneverdawn@users.noreply.github.com>
2022-04-28 15:50:54 -0400

    strip down testchain runtime (#29)
    Co-authored-by: Hugo Lindsay <hugo@satoshipay.io>

--- testchain/runtime/src/lib.rs
+++ testchain/runtime/src/lib.rs
@@ -1,5 +1,3 @@
-//! The Substrate Node Template runtime. This can be compiled with `#[no_std]`, ready for Wasm.
-
 #![cfg_attr(not(feature = "std"), no_std)]
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
@@ -9,54 +7,15 @@
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
 use frame_support::{
-	traits::{ConstU128, ConstU32, Contains, Currency as PalletCurrency, Imbalance, OnUnbalanced},
-	PalletId,
-};
-use frame_system::{
-	limits::{BlockLength, BlockWeights},
-	EnsureRoot,
+	construct_runtime, parameter_types,
+	traits::{ConstU128, ConstU8, Contains, KeyOwnerProofSystem},
+	weights::{constants::WEIGHT_PER_SECOND, IdentityFee},
 };
 use orml_currencies::BasicCurrencyAdapter;
 use orml_traits::parameter_type_with_key;
-use pallet_transaction_payment::{Multiplier, TargetedFeeAdjustment};
-use sp_api::impl_runtime_apis;
-use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
-use sp_runtime::{
-	create_runtime_str, generic, impl_opaque_keys,
-	traits::{
-		AccountIdConversion, BlakeTwo256, Block as BlockT, Convert, IdentityLookup, NumberFor, Zero,
-	},
-	transaction_validity::{TransactionSource, TransactionValidity},
-	ApplyExtrinsicResult, FixedPointNumber, Perquintill,
-};
-use sp_std::{marker::PhantomData, prelude::*};
-#[cfg(feature = "std")]
-use sp_version::NativeVersion;
-use sp_version::RuntimeVersion;
-
-// A few exports that help ease life for downstream crates.
-pub use frame_support::{
-	construct_runtime, parameter_types,
-	traits::{
-		EqualPrivilegeOnly, Everything, FindAuthor, Get, KeyOwnerProofSystem, LockIdentifier,
-	},
-	weights::{
-		constants::{BlockExecutionWeight, ExtrinsicBaseWeight, RocksDbWeight, WEIGHT_PER_SECOND},
-		DispatchClass, IdentityFee, Weight,
-	},
-	StorageValue,
-};
-pub use pallet_timestamp::Call as TimestampCall;
-#[cfg(any(feature = "std", test))]
-pub use sp_runtime::BuildStorage;
-pub use sp_runtime::{Perbill, Permill};
-
-pub use pallet_grandpa::{
+use pallet_grandpa::{
 	fg_primitives, AuthorityId as GrandpaId, AuthorityList as GrandpaAuthorityList,
 };
-pub use sp_consensus_aura::ed25519::AuthorityId as AuraId;
-
-pub use pallet_spacewalk::currency::CurrencyId;
 use pallet_spacewalk::{
 	address_conv::AddressConversion as StellarAddressConversion,
 	balance_conv::BalanceConversion as StellarBalanceConversion,
@@ -65,9 +24,22 @@ use pallet_spacewalk::{
 		StringCurrencyConversion as StellarStringCurrencyConversion,
 	},
 };
+use sp_api::impl_runtime_apis;
+use sp_consensus_aura::ed25519::AuthorityId as AuraId;
+use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
+use sp_runtime::{
+	create_runtime_str, generic, impl_opaque_keys,
+	traits::{BlakeTwo256, Block as BlockT, IdentityLookup, NumberFor, Zero},
+	transaction_validity::{TransactionSource, TransactionValidity},
+	ApplyExtrinsicResult, Perbill,
+};
+use sp_std::prelude::*;
+#[cfg(feature = "std")]
+use sp_version::NativeVersion;
+use sp_version::RuntimeVersion;
 
-pub use pallet_spacewalk;
-
+// A few exports that help ease life for downstream crates.
+pub use pallet_spacewalk::{self, currency::CurrencyId};
 pub use primitives::{
 	self, AccountId, Balance, BlockNumber, Hash, Moment, Nonce, Signature, SignedFixedPoint,
 	SignedInner, UnsignedFixedPoint, UnsignedInner,
@@ -94,29 +66,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 };
 
 pub const MILLISECS_PER_BLOCK: u64 = 6000;
-
 pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
-
-// These time units are defined in number of blocks.
-pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
-pub const HOURS: BlockNumber = MINUTES * 60;
-pub const DAYS: BlockNumber = HOURS * 24;
-pub const WEEKS: BlockNumber = DAYS * 7;
-pub const YEARS: BlockNumber = DAYS * 365;
-
-pub mod token_distribution {
-	use super::*;
-
-	// 1 billion INTR distributed over 4 years
-	// INTR has 10 decimal places, same as DOT
-	// See: https://wiki.polkadot.network/docs/learn-DOT#polkadot
-	pub const INITIAL_ALLOCATION: Balance = 1_000_000_000 * UNITS;
-
-	// multiplication is non-overflow by default
-	pub const ESCROW_INFLATION_REWARDS: Permill = Permill::from_parts(67000); // 6.7%
-	pub const TREASURY_INFLATION_REWARDS: Permill = Permill::from_parts(533000); // 53.3%
-	pub const VAULT_INFLATION_REWARDS: Permill = Permill::from_percent(40); // 40%
-}
 
 /// The version information used to identify this runtime when compiled natively.
 #[cfg(feature = "std")]
@@ -124,38 +74,16 @@ pub fn native_version() -> NativeVersion {
 	NativeVersion { runtime_version: VERSION, can_author_with: Default::default() }
 }
 
-/// We assume that ~10% of the block weight is consumed by `on_initalize` handlers.
-/// This is used to limit the maximal weight of a single extrinsic.
-const AVERAGE_ON_INITIALIZE_RATIO: Perbill = Perbill::from_percent(10);
-/// We allow `Normal` extrinsics to fill up the block up to 75%, the rest can be used
-/// by  Operational  extrinsics.
 const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
-/// We allow for 2 seconds of compute with a 6 second average block time.
-const MAXIMUM_BLOCK_WEIGHT: Weight = 2 * WEIGHT_PER_SECOND;
 
 parameter_types! {
-	pub const BlockHashCount: BlockNumber = 250;
 	pub const Version: RuntimeVersion = VERSION;
-	pub RuntimeBlockLength: BlockLength =
-		BlockLength::max_with_normal_ratio(5 * 1024 * 1024, NORMAL_DISPATCH_RATIO);
-	pub RuntimeBlockWeights: BlockWeights = BlockWeights::builder()
-		.base_block(BlockExecutionWeight::get())
-		.for_class(DispatchClass::all(), |weights| {
-			weights.base_extrinsic = ExtrinsicBaseWeight::get();
-		})
-		.for_class(DispatchClass::Normal, |weights| {
-			weights.max_total = Some(NORMAL_DISPATCH_RATIO * MAXIMUM_BLOCK_WEIGHT);
-		})
-		.for_class(DispatchClass::Operational, |weights| {
-			weights.max_total = Some(MAXIMUM_BLOCK_WEIGHT);
-			// Operational transactions have some extra reserved space, so that they
-			// are included even if block reached `MAXIMUM_BLOCK_WEIGHT`.
-			weights.reserved = Some(
-				MAXIMUM_BLOCK_WEIGHT - NORMAL_DISPATCH_RATIO * MAXIMUM_BLOCK_WEIGHT
-			);
-		})
-		.avg_block_initialization(AVERAGE_ON_INITIALIZE_RATIO)
-		.build_or_panic();
+	pub const BlockHashCount: BlockNumber = 250;
+	/// We allow for 2 seconds of compute with a 6 second average block time.
+	pub BlockWeights: frame_system::limits::BlockWeights = frame_system::limits::BlockWeights
+		::with_sensible_defaults(2 * WEIGHT_PER_SECOND, NORMAL_DISPATCH_RATIO);
+	pub BlockLength: frame_system::limits::BlockLength = frame_system::limits::BlockLength
+		::max_with_normal_ratio(5 * 1024 * 1024, NORMAL_DISPATCH_RATIO);
 	pub const SS58Prefix: u8 = 42;
 }
 
@@ -192,34 +120,11 @@ impl frame_system::Config for Runtime {
 	type DbWeight = ();
 	type BaseCallFilter = frame_support::traits::Everything;
 	type SystemWeightInfo = ();
-	type BlockWeights = RuntimeBlockWeights;
-	type BlockLength = RuntimeBlockLength;
+	type BlockWeights = BlockWeights;
+	type BlockLength = BlockLength;
 	type SS58Prefix = SS58Prefix;
 	type OnSetCode = ();
 	type MaxConsumers = frame_support::traits::ConstU32<16>;
-}
-
-parameter_types! {
-	pub const UncleGenerations: u32 = 0;
-}
-
-impl pallet_authorship::Config for Runtime {
-	type FindAuthor = AuraAccountAdapter;
-	type UncleGenerations = UncleGenerations;
-	type FilterUncle = ();
-	type EventHandler = ();
-}
-
-pub struct AuraAccountAdapter;
-
-impl FindAuthor<AccountId> for AuraAccountAdapter {
-	fn find_author<'a, I>(digests: I) -> Option<AccountId>
-	where
-		I: 'a + IntoIterator<Item = (sp_runtime::ConsensusEngineId, &'a [u8])>,
-	{
-		pallet_aura::AuraAuthorId::<Runtime>::find_author(digests)
-			.and_then(|k| AccountId::try_from(k.as_ref()).ok())
-	}
 }
 
 parameter_types! {
@@ -259,202 +164,21 @@ impl pallet_timestamp::Config for Runtime {
 	type WeightInfo = ();
 }
 
-pub type SlowAdjustingFeeUpdate<R> =
-	TargetedFeeAdjustment<R, TargetBlockFullness, AdjustmentVariable, MinimumMultiplier>;
-
-parameter_types! {
-	pub const TransactionByteFee: Balance = MILLICENTS;
-	/// This value increases the priority of `Operational` transactions by adding
-	/// a "virtual tip" that's equal to the `OperationalFeeMultiplier * final_fee`.
-	pub OperationalFeeMultiplier: u8 = 5;
-	/// The portion of the `NORMAL_DISPATCH_RATIO` that we adjust the fees with. Blocks filled less
-	/// than this will decrease the weight and more will increase.
-	pub const TargetBlockFullness: Perquintill = Perquintill::from_percent(25);
-	/// The adjustment variable of the runtime. Higher values will cause `TargetBlockFullness` to
-	/// change the fees more rapidly.
-	pub AdjustmentVariable: Multiplier = Multiplier::saturating_from_rational(3, 100_000);
-	/// Minimum amount of the multiplier. This value cannot be too low. A test case should ensure
-	/// that combined with `AdjustmentVariable`, we can recover from the minimum.
-	/// See `multiplier_can_grow_from_zero`.
-	pub MinimumMultiplier: Multiplier = Multiplier::saturating_from_rational(1u128, 1_000_000u128);
-}
-
-type NegativeImbalance<T, GetCurrencyId> =
-	<orml_tokens::CurrencyAdapter<T, GetCurrencyId> as PalletCurrency<
-		<T as frame_system::Config>::AccountId,
-	>>::NegativeImbalance;
-
-pub struct DealWithFees<T, GetCurrencyId>(PhantomData<(T, GetCurrencyId)>);
-
-impl<T, GetCurrencyId> OnUnbalanced<NegativeImbalance<T, GetCurrencyId>>
-	for DealWithFees<T, GetCurrencyId>
-where
-	T: pallet_authorship::Config + orml_tokens::Config,
-	GetCurrencyId: Get<T::CurrencyId>,
-{
-	fn on_unbalanceds<B>(
-		mut fees_then_tips: impl Iterator<Item = NegativeImbalance<T, GetCurrencyId>>,
-	) {
-		if let Some(mut fees) = fees_then_tips.next() {
-			if let Some(tips) = fees_then_tips.next() {
-				tips.merge_into(&mut fees);
-			}
-			if let Some(author) = pallet_authorship::Pallet::<T>::author() {
-				orml_tokens::CurrencyAdapter::<T, GetCurrencyId>::resolve_creating(&author, fees);
-			}
-		}
-	}
-}
-
 parameter_types! {
 	pub const GetNativeCurrencyId: CurrencyId = CurrencyId::Native;
 }
 
-type NativeCurrency = orml_tokens::CurrencyAdapter<Runtime, GetNativeCurrencyId>;
-
 impl pallet_transaction_payment::Config for Runtime {
 	type OnChargeTransaction = pallet_transaction_payment::CurrencyAdapter<Balances, ()>;
-	type TransactionByteFee = TransactionByteFee;
+	type TransactionByteFee = ConstU128<100_000>;
 	type WeightToFee = IdentityFee<Balance>;
-	type FeeMultiplierUpdate = SlowAdjustingFeeUpdate<Self>;
-	type OperationalFeeMultiplier = OperationalFeeMultiplier;
+	type FeeMultiplierUpdate = ();
+	type OperationalFeeMultiplier = ConstU8<5>;
 }
 
 impl pallet_sudo::Config for Runtime {
 	type Call = Call;
 	type Event = Event;
-}
-
-impl pallet_utility::Config for Runtime {
-	type Call = Call;
-	type Event = Event;
-	type WeightInfo = ();
-	type PalletsOrigin = OriginCaller;
-}
-
-parameter_types! {
-	pub MaximumSchedulerWeight: Weight = Perbill::from_percent(10) * RuntimeBlockWeights::get().max_block;
-	pub const MaxScheduledPerBlock: u32 = 30;
-	// Retry a scheduled item every 25 blocks (5 minute) until the preimage exists.
-	pub const NoPreimagePostponement: Option<u32> = Some(5 * MINUTES);
-}
-
-impl pallet_scheduler::Config for Runtime {
-	type Event = Event;
-	type Origin = Origin;
-	type PalletsOrigin = OriginCaller;
-	type Call = Call;
-	type MaximumWeight = MaximumSchedulerWeight;
-	type ScheduleOrigin = EnsureRoot<AccountId>;
-	type MaxScheduledPerBlock = MaxScheduledPerBlock;
-	type WeightInfo = ();
-	type OriginPrivilegeCmp = EqualPrivilegeOnly;
-	type PreimageProvider = Preimage;
-	type NoPreimagePostponement = NoPreimagePostponement;
-}
-
-parameter_types! {
-	pub const PreimageMaxSize: u32 = 4096 * 1024;
-	pub PreimageBaseDepositz: Balance = deposit(2, 64); // todo: rename
-	pub PreimageByteDepositz: Balance = deposit(0, 1);
-}
-
-impl pallet_preimage::Config for Runtime {
-	type WeightInfo = ();
-	type Event = Event;
-	type Currency = NativeCurrency;
-	type ManagerOrigin = EnsureRoot<AccountId>;
-	type MaxSize = PreimageMaxSize;
-	type BaseDeposit = PreimageBaseDepositz;
-	type ByteDeposit = PreimageByteDepositz;
-}
-
-// Migration for scheduler pallet to move from a plain Call to a CallOrHash.
-pub struct SchedulerMigrationV3;
-impl frame_support::traits::OnRuntimeUpgrade for SchedulerMigrationV3 {
-	fn on_runtime_upgrade() -> frame_support::weights::Weight {
-		Scheduler::migrate_v2_to_v3()
-	}
-
-	#[cfg(feature = "try-runtime")]
-	fn pre_upgrade() -> Result<(), &'static str> {
-		Scheduler::pre_migrate_to_v3()
-	}
-
-	#[cfg(feature = "try-runtime")]
-	fn post_upgrade() -> Result<(), &'static str> {
-		Scheduler::post_migrate_to_v3()
-	}
-}
-
-parameter_types! {
-	// One storage item; key size is 32; value is size 4+4+16+32 bytes = 56 bytes.
-	pub const GetDepositBase: Balance = deposit(1, 88);
-	// Additional storage item size of 32 bytes.
-	pub const GetDepositFactor: Balance = deposit(0, 32);
-	pub GetMaxSignatories: u16 = 100; // multisig of at most 100 accounts
-}
-
-impl pallet_multisig::Config for Runtime {
-	type Event = Event;
-	type Call = Call;
-	type Currency = NativeCurrency;
-	type DepositBase = GetDepositBase;
-	type DepositFactor = GetDepositFactor;
-	type MaxSignatories = GetMaxSignatories;
-	type WeightInfo = ();
-}
-
-// https://github.com/paritytech/polkadot/blob/ece7544b40d8b29897f5aa799f27840dcc32f24d/runtime/polkadot/src/constants.rs#L18
-pub const UNITS: Balance = 10_000_000_000;
-pub const DOLLARS: Balance = UNITS; // 10_000_000_000
-pub const CENTS: Balance = UNITS / 100; // 100_000_000
-pub const MILLICENTS: Balance = CENTS / 1_000; // 100_000
-
-pub const fn deposit(items: u32, bytes: u32) -> Balance {
-	items as Balance * 20 * DOLLARS + (bytes as Balance) * 100 * MILLICENTS
-}
-// Pallet accounts
-parameter_types! {
-	pub const FeePalletId: PalletId = PalletId(*b"mod/fees");
-	pub const SupplyPalletId: PalletId = PalletId(*b"mod/supl");
-	pub const EscrowAnnuityPalletId: PalletId = PalletId(*b"esc/annu");
-	pub const VaultAnnuityPalletId: PalletId = PalletId(*b"vlt/annu");
-	pub const TreasuryPalletId: PalletId = PalletId(*b"mod/trsy");
-	pub const VaultRegistryPalletId: PalletId = PalletId(*b"mod/vreg");
-}
-
-parameter_types! {
-	// 5EYCAe5i8QbRr5WN1PvaAVqPbfXsqazk9ocaxuzcTjgXPM1e
-	pub FeeAccount: AccountId = FeePalletId::get().into_account();
-	// 5EYCAe5i8QbRrUhwETaRvgif6H3HA84YQri7wjgLtKzRJCML
-	pub SupplyAccount: AccountId = SupplyPalletId::get().into_account();
-	// 5EYCAe5gXcgF6fT7oVsD7E4bfnRZeovzMUD2wkdyvCHrYQQE
-	pub EscrowAnnuityAccount: AccountId = EscrowAnnuityPalletId::get().into_account();
-	// 5EYCAe5jvsMTc6NLhunLTPVjJg5cZNweWKjNXKqz9RUqQJDY
-	pub VaultAnnuityAccount: AccountId = VaultAnnuityPalletId::get().into_account();
-	// 5EYCAe5i8QbRrWTk2CHjZA79gSf1piYSGm2LQfxaw6id3M88
-	pub TreasuryAccount: AccountId = TreasuryPalletId::get().into_account();
-	// 5EYCAe5i8QbRra1jndPz1WAuf1q1KHQNfu2cW1EXJ231emTd
-	pub VaultRegistryAccount: AccountId = VaultRegistryPalletId::get().into_account();
-}
-
-pub fn get_all_module_accounts() -> Vec<AccountId> {
-	vec![
-		FeeAccount::get(),
-		SupplyAccount::get(),
-		EscrowAnnuityAccount::get(),
-		VaultAnnuityAccount::get(),
-		TreasuryAccount::get(),
-		VaultRegistryAccount::get(),
-	]
-}
-
-pub struct DustRemovalWhitelist;
-impl Contains<AccountId> for DustRemovalWhitelist {
-	fn contains(a: &AccountId) -> bool {
-		get_all_module_accounts().contains(a)
-	}
 }
 
 parameter_types! {
@@ -474,33 +198,18 @@ impl orml_tokens::Config for Runtime {
 	type CurrencyId = CurrencyId;
 	type WeightInfo = ();
 	type ExistentialDeposits = ExistentialDeposits;
-	type OnDust = orml_tokens::TransferDust<Runtime, FeeAccount>;
+	type OnDust = orml_tokens::BurnDust<Runtime>;
 	type MaxLocks = MaxLocks;
 	type DustRemovalWhitelist = DustRemovalWhitelist;
 }
 
-parameter_types! {
-	pub const Span: BlockNumber = WEEKS;
-	pub const MaxPeriod: BlockNumber = WEEKS * 52 * 4;
-}
-
-pub struct BlockNumberToBalance;
-
-impl Convert<BlockNumber, Balance> for BlockNumberToBalance {
-	fn convert(a: BlockNumber) -> Balance {
-		a.into()
+pub struct DustRemovalWhitelist;
+impl Contains<AccountId> for DustRemovalWhitelist {
+	fn contains(a: &AccountId) -> bool {
+		vec![].contains(a)
 	}
 }
 
-impl<C> frame_system::offchain::SendTransactionTypes<C> for Runtime
-where
-	Call: From<C>,
-{
-	type OverarchingCall = Call;
-	type Extrinsic = UncheckedExtrinsic;
-}
-
-/// Configure the spacewalk pallet
 impl pallet_spacewalk::Config for Runtime {
 	type Event = Event;
 	type Call = Call;
@@ -520,12 +229,10 @@ impl orml_currencies::Config for Runtime {
 }
 
 impl pallet_balances::Config for Runtime {
-	type MaxLocks = ConstU32<50>;
+	type MaxLocks = MaxLocks;
 	type MaxReserves = ();
 	type ReserveIdentifier = [u8; 8];
-	/// The type for recording an account's balance.
 	type Balance = Balance;
-	/// The ubiquitous event type.
 	type Event = Event;
 	type DustRemoval = ();
 	type ExistentialDeposit = ConstU128<500>;
@@ -539,25 +246,16 @@ construct_runtime! {
 		NodeBlock = primitives::Block,
 		UncheckedExtrinsic = UncheckedExtrinsic
 	{
-		System: frame_system::{Pallet, Call, Storage, Config, Event<T>} = 0,
-		Timestamp: pallet_timestamp::{Pallet, Call, Storage, Inherent} = 1,
-		Sudo: pallet_sudo::{Pallet, Call, Storage, Config<T>, Event<T>} = 2,
-		Utility: pallet_utility::{Pallet, Call, Event} = 3,
-		TransactionPayment: pallet_transaction_payment::{Pallet, Storage} = 4,
-		Scheduler: pallet_scheduler::{Pallet, Call, Storage, Event<T>} = 5,
-		Preimage: pallet_preimage::{Pallet, Call, Storage, Event<T>} = 6,
-		MultiSig: pallet_multisig::{Pallet, Call, Storage, Event<T>} = 7,
-
-		// Tokens & Balances
-		Tokens: orml_tokens::{Pallet, Call, Storage, Config<T>, Event<T>} = 9,
-
-		Spacewalk: pallet_spacewalk::{Pallet, Call, Storage, Event<T>} = 40,
-		Currencies: orml_currencies::{Pallet, Call, Storage, Event<T>} = 41,
-		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>} = 42,
-
-		Authorship: pallet_authorship::{Pallet, Call, Storage} = 33,
-		Aura: pallet_aura::{Pallet, Config<T>} = 34,
-		Grandpa: pallet_grandpa::{Pallet, Call, Storage, Config, Event} = 35,
+		System: frame_system::{Pallet, Call, Storage, Config, Event<T>},
+		Timestamp: pallet_timestamp::{Pallet, Call, Storage, Inherent},
+		Aura: pallet_aura::{Pallet, Config<T>},
+		Grandpa: pallet_grandpa::{Pallet, Call, Storage, Config, Event},
+		Sudo: pallet_sudo::{Pallet, Call, Storage, Config<T>, Event<T>},
+		Tokens: orml_tokens::{Pallet, Call, Storage, Config<T>, Event<T>},
+		Spacewalk: pallet_spacewalk::{Pallet, Call, Storage, Event<T>},
+		Currencies: orml_currencies::{Pallet, Call, Storage, Event<T>},
+		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+		TransactionPayment: pallet_transaction_payment::{Pallet, Storage},
 	}
 }
 
@@ -734,11 +432,8 @@ impl_runtime_apis! {
 		) {
 			use frame_benchmarking::{list_benchmark, Benchmarking, BenchmarkList};
 			use frame_support::traits::StorageInfoTrait;
-
 			let mut list = Vec::<BenchmarkList>::new();
-
 			let storage_info = AllPalletsWithSystem::storage_info();
-
 			return (list, storage_info)
 		}
 
@@ -746,7 +441,6 @@ impl_runtime_apis! {
 			config: frame_benchmarking::BenchmarkConfig
 		) -> Result<Vec<frame_benchmarking::BenchmarkBatch>, sp_runtime::RuntimeString> {
 			use frame_benchmarking::{Benchmarking, BenchmarkBatch, add_benchmark, TrackedStorageKey};
-
 			let whitelist: Vec<TrackedStorageKey> = vec![
 				// Block Number
 				hex_literal::hex!("26aa394eea5630e07c48ae0c9558cef702a5c1b19ab7a04f536c519aca4983ac").to_vec().into(),
@@ -759,10 +453,8 @@ impl_runtime_apis! {
 				// System Events
 				hex_literal::hex!("26aa394eea5630e07c48ae0c9558cef780d41e5e16056765bc8461851072c9d7").to_vec().into(),
 			];
-
 			let mut batches = Vec::<BenchmarkBatch>::new();
 			let params = (&config, &whitelist);
-
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
 			Ok(batches)
 		}
