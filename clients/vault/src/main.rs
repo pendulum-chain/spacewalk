003c9136ec3c1e27384f0b111640fc59c5aee923
Gonza <gonzalo@satoshipay.io>
2022-09-28 11:08:33 +0200

    cargo fmt

--- clients/vault/src/main.rs
+++ clients/vault/src/main.rs
@@ -7,50 +7,50 @@ use vault::{Error, VaultService, VaultServiceConfig, ABOUT, AUTHORS, NAME, VERSI
 #[derive(Parser, Debug, Clone)]
 #[clap(name = NAME, version = VERSION, author = AUTHORS, about = ABOUT)]
 pub struct Opts {
-    /// Keyring / keyfile options.
-    #[clap(flatten)]
-    pub account_info: runtime::cli::ProviderUserOpts,
+	/// Keyring / keyfile options.
+	#[clap(flatten)]
+	pub account_info: runtime::cli::ProviderUserOpts,
 
-    /// Connection settings for the BTC Parachain.
-    #[clap(flatten)]
-    pub parachain: runtime::cli::ConnectionOpts,
+	/// Connection settings for the BTC Parachain.
+	#[clap(flatten)]
+	pub parachain: runtime::cli::ConnectionOpts,
 
-    /// Settings specific to the vault client.
-    #[clap(flatten)]
-    pub vault: VaultServiceConfig,
+	/// Settings specific to the vault client.
+	#[clap(flatten)]
+	pub vault: VaultServiceConfig,
 
-    /// General service settings.
-    #[clap(flatten)]
-    pub service: ServiceConfig,
+	/// General service settings.
+	#[clap(flatten)]
+	pub service: ServiceConfig,
 }
 
 async fn start() -> Result<(), Error> {
-    let opts: Opts = Opts::parse();
-    opts.service.logging_format.init_subscriber();
+	let opts: Opts = Opts::parse();
+	opts.service.logging_format.init_subscriber();
 
-    let (pair, wallet_name) = opts.account_info.get_key_pair()?;
-    let signer = SpacewalkSigner::new(pair);
+	let (pair, wallet_name) = opts.account_info.get_key_pair()?;
+	let signer = SpacewalkSigner::new(pair);
 
-    ConnectionManager::<_, VaultService>::new(
-        signer.clone(),
-        Some(wallet_name.to_string()),
-        opts.parachain,
-        opts.service,
-        opts.vault,
-    )
-    .start()
-    .await?;
+	ConnectionManager::<_, VaultService>::new(
+		signer.clone(),
+		Some(wallet_name.to_string()),
+		opts.parachain,
+		opts.service,
+		opts.vault,
+	)
+	.start()
+	.await?;
 
-    Ok(())
+	Ok(())
 }
 
 #[tokio::main]
 async fn main() {
-    let exit_code = if let Err(err) = start().await {
-        eprintln!("Error: {}", err);
-        1
-    } else {
-        0
-    };
-    std::process::exit(exit_code);
+	let exit_code = if let Err(err) = start().await {
+		eprintln!("Error: {}", err);
+		1
+	} else {
+		0
+	};
+	std::process::exit(exit_code);
 }
