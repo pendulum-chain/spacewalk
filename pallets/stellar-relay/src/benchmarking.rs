05a34cf8a862c8a9441dece09043134af382f6f1
Marcel Ebert <mail@marcel-ebert.de>
2022-10-05 15:14:34 +0200

    Add benchmark

--- pallets/stellar-relay/src/benchmarking.rs
+++ pallets/stellar-relay/src/benchmarking.rs
@@ -1,20 +1,41 @@
 //! Benchmarking setup for pallet-template
 
-use super::*;
-
-#[allow(unused)]
-use crate::Pallet as Template;
-use frame_benchmarking::{benchmarks, whitelisted_caller};
+use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
+use frame_support::BoundedVec;
 use frame_system::RawOrigin;
 
-benchmarks! {
-	do_something {
-		let s in 0 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), s)
-	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
-	}
+use crate::traits::{FieldLength, Organization, Validator};
+#[allow(unused)]
+use crate::Pallet as StellarRelay;
 
-	impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
+use super::*;
+
+benchmarks! {
+	update_tier_1_validator_set_benchmark {
+		let caller: T::AccountId = whitelisted_caller();
+
+		let bounded_vec = BoundedVec::<u8, FieldLength>::default();
+
+		let validator: Validator = Validator {
+			name: bounded_vec.clone(),
+			public_key: bounded_vec.clone(),
+			organization_id: 0,
+		};
+
+		let validators = vec![validator; 255];
+
+		let organization: Organization = Organization {
+			id: 0,
+			name: bounded_vec.clone(),
+		};
+
+		let organizations = vec![organization; 255];
+
+	}: update_tier_1_validator_set(RawOrigin::Root, validators.clone(), organizations.clone())
+	verify {
+		assert_eq!(Organizations::<T>::get(), BoundedVec::<Organization, T::OrganizationLimit>::try_from(organizations).unwrap());
+		assert_eq!(Validators::<T>::get(), BoundedVec::<Validator, T::ValidatorLimit>::try_from(validators).unwrap());
+	}
 }
+
+impl_benchmark_test_suite!(StellarRelay, crate::mock::new_test_ext(), crate::mock::Test);
