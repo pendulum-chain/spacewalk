980f02829defe01e461452b220bf20f760144234
Marcel Ebert <mail@marcel-ebert.de>
2022-10-10 10:29:32 +0200

    Add default genesis for stellar network

--- pallets/stellar-relay/src/lib.rs
+++ pallets/stellar-relay/src/lib.rs
@@ -123,7 +123,281 @@ pub mod pallet {
 	#[cfg(feature = "std")]
 	impl<T: Config> Default for GenesisConfig<T> {
 		fn default() -> Self {
-			Self { validators: vec![], organizations: vec![], phantom: Default::default() }
+			// Create public network organizations
+			let organization_sdf = OrganizationOf::<T> {
+				name: create_bounded_vec("Stellar Development Foundation"),
+				id: 0.into(),
+				public_network: true,
+			};
+			let organization_satoshipay = OrganizationOf::<T> {
+				name: create_bounded_vec("SatoshiPay"),
+				id: 1.into(),
+				public_network: true,
+			};
+			let organization_wirex = OrganizationOf::<T> {
+				name: create_bounded_vec("Wirex"),
+				id: 2.into(),
+				public_network: true,
+			};
+			let organization_coinqvest = OrganizationOf::<T> {
+				name: create_bounded_vec("Coinqvest"),
+				id: 3.into(),
+				public_network: true,
+			};
+			let organization_blockdaemon = OrganizationOf::<T> {
+				name: create_bounded_vec("Blockdaemon"),
+				id: 4.into(),
+				public_network: true,
+			};
+			let organization_lobstr = OrganizationOf::<T> {
+				name: create_bounded_vec("LOBSTR"),
+				id: 5.into(),
+				public_network: true,
+			};
+			let organization_public_node = OrganizationOf::<T> {
+				name: create_bounded_vec("Public Node"),
+				id: 6.into(),
+				public_network: true,
+			};
+
+			// Create test network organizations
+			let organization_testnet_sdf = OrganizationOf::<T> {
+				name: create_bounded_vec("sdftest"),
+				id: 7.into(),
+				public_network: false,
+			};
+
+			let validators: Vec<ValidatorOf<T>> = vec![
+				// Satoshipay validators
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$satoshipay-us"),
+					public_key: create_bounded_vec(
+						"GAK6Z5UVGUVSEK6PEOCAYJISTT5EJBB34PN3NOLEQG2SUKXRVV2F6HZY",
+					),
+					organization_id: organization_satoshipay.id,
+					public_network: organization_satoshipay.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$satoshipay-de"),
+					public_key: create_bounded_vec(
+						"GC5SXLNAM3C4NMGK2PXK4R34B5GNZ47FYQ24ZIBFDFOCU6D4KBN4POAE",
+					),
+					organization_id: organization_satoshipay.id,
+					public_network: organization_satoshipay.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$satoshipay-sg"),
+					public_key: create_bounded_vec(
+						"GBJQUIXUO4XSNPAUT6ODLZUJRV2NPXYASKUBY4G5MYP3M47PCVI55MNT",
+					),
+					organization_id: organization_satoshipay.id,
+					public_network: organization_satoshipay.public_network,
+				},
+				// SDF validators
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$sdf1"),
+					public_key: create_bounded_vec(
+						"GCGB2S2KGYARPVIA37HYZXVRM2YZUEXA6S33ZU5BUDC6THSB62LZSTYH",
+					),
+					organization_id: organization_sdf.id,
+					public_network: organization_sdf.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$sdf2"),
+					public_key: create_bounded_vec(
+						"GCM6QMP3DLRPTAZW2UZPCPX2LF3SXWXKPMP3GKFZBDSF3QZGV2G5QSTK",
+					),
+					organization_id: organization_sdf.id,
+					public_network: organization_sdf.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$sdf3"),
+					public_key: create_bounded_vec(
+						"GABMKJM6I25XI4K7U6XWMULOUQIQ27BCTMLS6BYYSOWKTBUXVRJSXHYQ",
+					),
+					organization_id: organization_sdf.id,
+					public_network: organization_sdf.public_network,
+				},
+				// Wirex validators
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$wirex-sg"),
+					public_key: create_bounded_vec(
+						"GAB3GZIE6XAYWXGZUDM4GMFFLJBFMLE2JDPUCWUZXMOMT3NHXDHEWXAS",
+					),
+					organization_id: organization_wirex.id,
+					public_network: organization_wirex.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$wirex-us"),
+					public_key: create_bounded_vec(
+						"GDXUKFGG76WJC7ACEH3JUPLKM5N5S76QSMNDBONREUXPCZYVPOLFWXUS",
+					),
+					organization_id: organization_wirex.id,
+					public_network: organization_wirex.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$wirex-uk"),
+					public_key: create_bounded_vec(
+						"GBBQQT3EIUSXRJC6TGUCGVA3FVPXVZLGG3OJYACWBEWYBHU46WJLWXEU",
+					),
+					organization_id: organization_wirex.id,
+					public_network: organization_wirex.public_network,
+				},
+				// Coinqvest validators
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$coinqvest-germany"),
+					public_key: create_bounded_vec(
+						"GD6SZQV3WEJUH352NTVLKEV2JM2RH266VPEM7EH5QLLI7ZZAALMLNUVN",
+					),
+					organization_id: organization_coinqvest.id,
+					public_network: organization_coinqvest.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$coinqvest-finland"),
+					public_key: create_bounded_vec(
+						"GADLA6BJK6VK33EM2IDQM37L5KGVCY5MSHSHVJA4SCNGNUIEOTCR6J5T",
+					),
+					organization_id: organization_coinqvest.id,
+					public_network: organization_coinqvest.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$coinqvest-hongkong"),
+					public_key: create_bounded_vec(
+						"GAZ437J46SCFPZEDLVGDMKZPLFO77XJ4QVAURSJVRZK2T5S7XUFHXI2Z",
+					),
+					organization_id: organization_coinqvest.id,
+					public_network: organization_coinqvest.public_network,
+				},
+				// Blockdaemon validators
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$blockdaemon1"),
+					public_key: create_bounded_vec(
+						"GAAV2GCVFLNN522ORUYFV33E76VPC22E72S75AQ6MBR5V45Z5DWVPWEU",
+					),
+					organization_id: organization_blockdaemon.id,
+					public_network: organization_blockdaemon.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$blockdaemon2"),
+					public_key: create_bounded_vec(
+						"GAVXB7SBJRYHSG6KSQHY74N7JAFRL4PFVZCNWW2ARI6ZEKNBJSMSKW7C",
+					),
+					organization_id: organization_blockdaemon.id,
+					public_network: organization_blockdaemon.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$blockdaemon3"),
+					public_key: create_bounded_vec(
+						"GAYXZ4PZ7P6QOX7EBHPIZXNWY4KCOBYWJCA4WKWRKC7XIUS3UJPT6EZ4",
+					),
+					organization_id: organization_blockdaemon.id,
+					public_network: organization_blockdaemon.public_network,
+				},
+				// LOBSTR validators
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$lobstr1"),
+					public_key: create_bounded_vec(
+						"GCFONE23AB7Y6C5YZOMKUKGETPIAJA4QOYLS5VNS4JHBGKRZCPYHDLW7",
+					),
+					organization_id: organization_lobstr.id,
+					public_network: organization_lobstr.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$lobstr2"),
+					public_key: create_bounded_vec(
+						"GDXQB3OMMQ6MGG43PWFBZWBFKBBDUZIVSUDAZZTRAWQZKES2CDSE5HKJ",
+					),
+					organization_id: organization_lobstr.id,
+					public_network: organization_lobstr.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$lobstr3"),
+					public_key: create_bounded_vec(
+						"GD5QWEVV4GZZTQP46BRXV5CUMMMLP4JTGFD7FWYJJWRL54CELY6JGQ63",
+					),
+					organization_id: organization_lobstr.id,
+					public_network: organization_lobstr.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$lobstr4"),
+					public_key: create_bounded_vec(
+						"GA7TEPCBDQKI7JQLQ34ZURRMK44DVYCIGVXQQWNSWAEQR6KB4FMCBT7J",
+					),
+					organization_id: organization_lobstr.id,
+					public_network: organization_lobstr.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$lobstr5"),
+					public_key: create_bounded_vec(
+						"GA5STBMV6QDXFDGD62MEHLLHZTPDI77U3PFOD2SELU5RJDHQWBR5NNK7",
+					),
+					organization_id: organization_lobstr.id,
+					public_network: organization_lobstr.public_network,
+				},
+				// Public Node validators
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$hercules"),
+					public_key: create_bounded_vec(
+						"GBLJNN3AVZZPG2FYAYTYQKECNWTQYYUUY2KVFN2OUKZKBULXIXBZ4FCT",
+					),
+					organization_id: organization_public_node.id,
+					public_network: organization_public_node.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$boötes"),
+					public_key: create_bounded_vec(
+						"GCVJ4Z6TI6Z2SOGENSPXDQ2U4RKH3CNQKYUHNSSPYFPNWTLGS6EBH7I2",
+					),
+					organization_id: organization_public_node.id,
+					public_network: organization_public_node.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$lyra"),
+					public_key: create_bounded_vec(
+						"GCIXVKNFPKWVMKJKVK2V4NK7D4TC6W3BUMXSIJ365QUAXWBRPPJXIR2Z",
+					),
+					organization_id: organization_public_node.id,
+					public_network: organization_public_node.public_network,
+				},
+				// Testnet validators
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$sdftest1"),
+					public_key: create_bounded_vec(
+						"GDKXE2OZMJIPOSLNA6N6F2BVCI3O777I2OOC4BV7VOYUEHYX7RTRYA7Y",
+					),
+					organization_id: organization_testnet_sdf.id,
+					public_network: organization_testnet_sdf.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$sdftest2"),
+					public_key: create_bounded_vec(
+						"GCUCJTIYXSOXKBSNFGNFWW5MUQ54HKRPGJUTQFJ5RQXZXNOLNXYDHRAP",
+					),
+					organization_id: organization_testnet_sdf.id,
+					public_network: organization_testnet_sdf.public_network,
+				},
+				ValidatorOf::<T> {
+					name: create_bounded_vec("$sdftest3"),
+					public_key: create_bounded_vec(
+						"GC2V2EFSXN6SQTWVYA5EPJPBWWIMSD2XQNKUOHGEKB535AQE2I6IXV2Z",
+					),
+					organization_id: organization_testnet_sdf.id,
+					public_network: organization_testnet_sdf.public_network,
+				},
+			];
+
+			let organizations: Vec<OrganizationOf<T>> = vec![
+				organization_satoshipay,
+				organization_sdf,
+				organization_wirex,
+				organization_coinqvest,
+				organization_blockdaemon,
+				organization_lobstr,
+				organization_public_node,
+				organization_testnet_sdf,
+			];
+
+			GenesisConfig { validators, organizations, phantom: Default::default() }
 		}
 	}
 
