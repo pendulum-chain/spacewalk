41ef7e1c6032731e79a1e4ff91074b49bcd91b63
Marcel Ebert <mail@marcel-ebert.de>
2022-10-12 09:17:03 +0200

    Add currency module

---
+++ pallets/currency/src/types.rs
@@ -0,0 +1,17 @@
+use frame_support::dispatch::DispatchError;
+
+use crate::Config;
+
+pub type CurrencyId<T> = <T as orml_tokens::Config>::CurrencyId;
+pub type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+
+pub(crate) type BalanceOf<T> = <T as Config>::Balance;
+
+pub(crate) type SignedFixedPoint<T> = <T as Config>::SignedFixedPoint;
+
+pub(crate) type UnsignedFixedPoint<T> = <T as Config>::UnsignedFixedPoint;
+
+pub(crate) type SignedInner<T> = <T as Config>::SignedInner;
+pub trait CurrencyConversion<Amount, CurrencyId> {
+	fn convert(amount: &Amount, to: CurrencyId) -> Result<Amount, DispatchError>;
+}
