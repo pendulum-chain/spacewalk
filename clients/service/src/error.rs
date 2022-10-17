458a257f12f3b2bdd69d40ab05404ff16f8b0e7e
Gonza Montiel <gonzamontiel@users.noreply.github.com>
2022-10-17 17:54:08 +0200

    96 add client folder to spacewalk root virtual manifest (#109)
    * move clients package to root cargo
    
    * cargo fmt
    
    * unify toolchains
    
    * unify fmt
    
    * cargo toml format
    
    * update cargo lock
    
    * cargo fmt
    
    * Add standalone-metadata as default feature
    
    * Update readme
    
    * update cargo lock
    
    * fixed missign retry variable

--- clients/service/src/error.rs
+++ clients/service/src/error.rs
@@ -5,22 +5,22 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("Received an invalid response")]
-    InvalidResponse,
-    #[error("Client has shutdown")]
-    ClientShutdown,
+	#[error("Received an invalid response")]
+	InvalidResponse,
+	#[error("Client has shutdown")]
+	ClientShutdown,
 
-    #[error("SerdeJsonError: {0}")]
-    SerdeJsonError(#[from] SerdeJsonError),
-    #[error("HyperError: {0}")]
-    HyperError(#[from] HyperError),
-    #[error("HyperHttpError: {0}")]
-    HyperHttpError(#[from] HyperHttpError),
+	#[error("SerdeJsonError: {0}")]
+	SerdeJsonError(#[from] SerdeJsonError),
+	#[error("HyperError: {0}")]
+	HyperError(#[from] HyperError),
+	#[error("HyperHttpError: {0}")]
+	HyperHttpError(#[from] HyperHttpError),
 
-    #[error("RuntimeError: {0}")]
-    RuntimeError(#[from] RuntimeError),
+	#[error("RuntimeError: {0}")]
+	RuntimeError(#[from] RuntimeError),
 
-    /// Other error
-    #[error("Other: {0}")]
-    Other(String),
+	/// Other error
+	#[error("Other: {0}")]
+	Other(String),
 }
--- clients/vault/src/error.rs
+++ clients/vault/src/error.rs
@@ -10,63 +10,63 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("Insufficient funds available")]
-    InsufficientFunds,
-    #[error("Value below dust amount")]
-    BelowDustAmount,
-    #[error("Transaction contains more than one return-to-self uxto")]
-    TooManyReturnToSelfAddresses,
-    #[error("Mathematical operation caused an overflow")]
-    ArithmeticOverflow,
-    #[error("Mathematical operation caused an underflow")]
-    ArithmeticUnderflow,
-    #[error(transparent)]
-    TryIntoIntError(#[from] std::num::TryFromIntError),
-    #[error("Deadline has expired")]
-    DeadlineExpired,
+	#[error("Insufficient funds available")]
+	InsufficientFunds,
+	#[error("Value below dust amount")]
+	BelowDustAmount,
+	#[error("Transaction contains more than one return-to-self uxto")]
+	TooManyReturnToSelfAddresses,
+	#[error("Mathematical operation caused an overflow")]
+	ArithmeticOverflow,
+	#[error("Mathematical operation caused an underflow")]
+	ArithmeticUnderflow,
+	#[error(transparent)]
+	TryIntoIntError(#[from] std::num::TryFromIntError),
+	#[error("Deadline has expired")]
+	DeadlineExpired,
 
-    #[error("ServiceError: {0}")]
-    ServiceError(#[from] ServiceError),
-    #[error("RPC error: {0}")]
-    RpcError(#[from] RpcError),
-    #[error("Hex conversion error: {0}")]
-    FromHexError(#[from] FromHexError),
-    #[error("RuntimeError: {0}")]
-    RuntimeError(#[from] RuntimeError),
-    #[error("SubxtError: {0}")]
-    SubxtError(#[from] SubxtError),
-    #[error("CodecError: {0}")]
-    CodecError(#[from] CodecError),
+	#[error("ServiceError: {0}")]
+	ServiceError(#[from] ServiceError),
+	#[error("RPC error: {0}")]
+	RpcError(#[from] RpcError),
+	#[error("Hex conversion error: {0}")]
+	FromHexError(#[from] FromHexError),
+	#[error("RuntimeError: {0}")]
+	RuntimeError(#[from] RuntimeError),
+	#[error("SubxtError: {0}")]
+	SubxtError(#[from] SubxtError),
+	#[error("CodecError: {0}")]
+	CodecError(#[from] CodecError),
 
-    #[error("Error returned when fetching remote info")]
-    HttpFetchingError,
-    #[error("Failed to post http request")]
-    HttpPostError,
-    #[error("Lookup Error")]
-    LookupError,
-    #[error("Stellar SDK Error")]
-    StellarSdkError,
-    #[error("ExceedsMaximumLengthError")]
-    ExceedsMaximumLengthError,
-    #[error("Failed to convert balance")]
-    BalanceConversionError,
-    #[error("Utf8Error: {0}")]
-    Utf8Error(#[from] Utf8Error),
-    #[error("Failed to parse sequence number")]
-    SeqNoParsingError,
+	#[error("Error returned when fetching remote info")]
+	HttpFetchingError,
+	#[error("Failed to post http request")]
+	HttpPostError,
+	#[error("Lookup Error")]
+	LookupError,
+	#[error("Stellar SDK Error")]
+	StellarSdkError,
+	#[error("ExceedsMaximumLengthError")]
+	ExceedsMaximumLengthError,
+	#[error("Failed to convert balance")]
+	BalanceConversionError,
+	#[error("Utf8Error: {0}")]
+	Utf8Error(#[from] Utf8Error),
+	#[error("Failed to parse sequence number")]
+	SeqNoParsingError,
 }
 
 impl From<StellarSdkError> for Error {
-    fn from(error: StellarSdkError) -> Self {
-        match error {
-            StellarSdkError::ExceedsMaximumLength { .. } => Self::ExceedsMaximumLengthError,
-            _ => Self::StellarSdkError,
-        }
-    }
+	fn from(error: StellarSdkError) -> Self {
+		match error {
+			StellarSdkError::ExceedsMaximumLength { .. } => Self::ExceedsMaximumLengthError,
+			_ => Self::StellarSdkError,
+		}
+	}
 }
 
 impl From<LookupError> for Error {
-    fn from(_: LookupError) -> Self {
-        Self::BalanceConversionError
-    }
+	fn from(_: LookupError) -> Self {
+		Self::BalanceConversionError
+	}
 }
