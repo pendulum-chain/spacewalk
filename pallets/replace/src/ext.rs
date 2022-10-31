3101805f3d7fe8a56db9ffa8ec68a9206327943f
Marcel Ebert <mail@marcel-ebert.de>
2022-10-31 17:05:53 +0100

    Add basics for replace pallet

---
+++ pallets/replace/src/ext.rs
@@ -0,0 +1,201 @@
+#[cfg(test)]
+use mocktopus::macros::mockable;
+
+#[cfg_attr(test, mockable)]
+pub(crate) mod btc_relay {
+	use bitcoin::types::{MerkleProof, Transaction, Value};
+	use btc_relay::BtcAddress;
+	use frame_support::dispatch::DispatchError;
+	use sp_core::H256;
+	use sp_std::convert::TryInto;
+
+	pub fn verify_and_validate_op_return_transaction<T: crate::Config, V: TryInto<Value>>(
+		merkle_proof: MerkleProof,
+		transaction: Transaction,
+		recipient_btc_address: BtcAddress,
+		expected_btc: V,
+		op_return_id: H256,
+	) -> Result<(), DispatchError> {
+		<btc_relay::Pallet<T>>::verify_and_validate_op_return_transaction(
+			merkle_proof,
+			transaction,
+			recipient_btc_address,
+			expected_btc,
+			op_return_id,
+		)
+	}
+
+	pub fn get_best_block_height<T: crate::Config>() -> u32 {
+		<btc_relay::Pallet<T>>::get_best_block_height()
+	}
+
+	pub fn parse_transaction<T: btc_relay::Config>(
+		raw_tx: &[u8],
+	) -> Result<Transaction, DispatchError> {
+		<btc_relay::Pallet<T>>::parse_transaction(raw_tx)
+	}
+
+	pub fn parse_merkle_proof<T: btc_relay::Config>(
+		raw_merkle_proof: &[u8],
+	) -> Result<MerkleProof, DispatchError> {
+		<btc_relay::Pallet<T>>::parse_merkle_proof(raw_merkle_proof)
+	}
+
+	pub fn has_request_expired<T: crate::Config>(
+		opentime: T::BlockNumber,
+		btc_open_height: u32,
+		period: T::BlockNumber,
+	) -> Result<bool, DispatchError> {
+		<btc_relay::Pallet<T>>::has_request_expired(opentime, btc_open_height, period)
+	}
+}
+
+#[cfg_attr(test, mockable)]
+pub(crate) mod vault_registry {
+	use crate::DefaultVaultId;
+	use currency::Amount;
+	use frame_support::dispatch::{DispatchError, DispatchResult};
+	use vault_registry::types::CurrencySource;
+
+	pub fn transfer_funds<T: crate::Config>(
+		from: CurrencySource<T>,
+		to: CurrencySource<T>,
+		amount: &Amount<T>,
+	) -> DispatchResult {
+		<vault_registry::Pallet<T>>::transfer_funds(from, to, amount)
+	}
+
+	pub fn replace_tokens<T: crate::Config>(
+		old_vault_id: &DefaultVaultId<T>,
+		new_vault_id: &DefaultVaultId<T>,
+		tokens: &Amount<T>,
+		collateral: &Amount<T>,
+	) -> DispatchResult {
+		<vault_registry::Pallet<T>>::replace_tokens(old_vault_id, new_vault_id, tokens, collateral)
+	}
+
+	pub fn cancel_replace_tokens<T: crate::Config>(
+		old_vault_id: &DefaultVaultId<T>,
+		new_vault_id: &DefaultVaultId<T>,
+		tokens: &Amount<T>,
+	) -> DispatchResult {
+		<vault_registry::Pallet<T>>::cancel_replace_tokens(old_vault_id, new_vault_id, tokens)
+	}
+
+	pub fn is_vault_liquidated<T: crate::Config>(
+		vault_id: &DefaultVaultId<T>,
+	) -> Result<bool, DispatchError> {
+		<vault_registry::Pallet<T>>::is_vault_liquidated(vault_id)
+	}
+
+	pub fn try_increase_to_be_redeemed_tokens<T: crate::Config>(
+		vault_id: &DefaultVaultId<T>,
+		tokens: &Amount<T>,
+	) -> DispatchResult {
+		<vault_registry::Pallet<T>>::try_increase_to_be_redeemed_tokens(vault_id, tokens)
+	}
+
+	pub fn ensure_not_banned<T: crate::Config>(vault_id: &DefaultVaultId<T>) -> DispatchResult {
+		<vault_registry::Pallet<T>>::_ensure_not_banned(vault_id)
+	}
+
+	pub fn try_increase_to_be_issued_tokens<T: crate::Config>(
+		vault_id: &DefaultVaultId<T>,
+		amount: &Amount<T>,
+	) -> Result<(), DispatchError> {
+		<vault_registry::Pallet<T>>::try_increase_to_be_issued_tokens(vault_id, amount)
+	}
+
+	pub fn requestable_to_be_replaced_tokens<T: crate::Config>(
+		vault_id: &DefaultVaultId<T>,
+	) -> Result<Amount<T>, DispatchError> {
+		<vault_registry::Pallet<T>>::requestable_to_be_replaced_tokens(vault_id)
+	}
+
+	pub fn try_increase_to_be_replaced_tokens<T: crate::Config>(
+		vault_id: &DefaultVaultId<T>,
+		amount: &Amount<T>,
+	) -> Result<Amount<T>, DispatchError> {
+		<vault_registry::Pallet<T>>::try_increase_to_be_replaced_tokens(vault_id, amount)
+	}
+
+	pub fn decrease_to_be_replaced_tokens<T: crate::Config>(
+		vault_id: &DefaultVaultId<T>,
+		tokens: &Amount<T>,
+	) -> Result<(Amount<T>, Amount<T>), DispatchError> {
+		<vault_registry::Pallet<T>>::decrease_to_be_replaced_tokens(vault_id, tokens)
+	}
+
+	pub fn withdraw_replace_request<T: crate::Config>(
+		vault_id: &DefaultVaultId<T>,
+		amount: &Amount<T>,
+	) -> Result<(Amount<T>, Amount<T>), DispatchError> {
+		<vault_registry::Pallet<T>>::withdraw_replace_request(vault_id, amount)
+	}
+
+	pub fn try_deposit_collateral<T: crate::Config>(
+		vault_id: &DefaultVaultId<T>,
+		amount: &Amount<T>,
+	) -> Result<(), DispatchError> {
+		<vault_registry::Pallet<T>>::try_deposit_collateral(vault_id, amount)
+	}
+
+	pub fn force_withdraw_collateral<T: crate::Config>(
+		vault_id: &DefaultVaultId<T>,
+		amount: &Amount<T>,
+	) -> Result<(), DispatchError> {
+		<vault_registry::Pallet<T>>::force_withdraw_collateral(vault_id, amount)
+	}
+
+	pub fn is_allowed_to_withdraw_collateral<T: crate::Config>(
+		vault_id: &DefaultVaultId<T>,
+		amount: &Amount<T>,
+	) -> Result<bool, DispatchError> {
+		<vault_registry::Pallet<T>>::is_allowed_to_withdraw_collateral(vault_id, amount)
+	}
+
+	pub fn calculate_collateral<T: crate::Config>(
+		collateral: &Amount<T>,
+		numerator: &Amount<T>,
+		denominator: &Amount<T>,
+	) -> Result<Amount<T>, DispatchError> {
+		<vault_registry::Pallet<T>>::calculate_collateral(collateral, numerator, denominator)
+	}
+}
+
+#[cfg_attr(test, mockable)]
+pub(crate) mod security {
+	use sp_core::H256;
+
+	pub fn get_secure_id<T: crate::Config>(id: &T::AccountId) -> H256 {
+		<security::Pallet<T>>::get_secure_id(id)
+	}
+
+	pub fn active_block_number<T: crate::Config>() -> T::BlockNumber {
+		<security::Pallet<T>>::active_block_number()
+	}
+}
+
+#[cfg_attr(test, mockable)]
+pub(crate) mod fee {
+	use currency::Amount;
+	use frame_support::dispatch::DispatchError;
+
+	pub fn get_replace_griefing_collateral<T: crate::Config>(
+		amount: &Amount<T>,
+	) -> Result<Amount<T>, DispatchError> {
+		<fee::Pallet<T>>::get_replace_griefing_collateral(amount)
+	}
+}
+
+#[cfg_attr(test, mockable)]
+pub(crate) mod nomination {
+	use crate::DefaultVaultId;
+	use sp_runtime::DispatchError;
+
+	pub fn is_nominatable<T: crate::Config>(
+		vault_id: &DefaultVaultId<T>,
+	) -> Result<bool, DispatchError> {
+		<nomination::Pallet<T>>::is_opted_in(vault_id)
+	}
+}
