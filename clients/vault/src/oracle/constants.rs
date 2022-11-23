1d7104d7e0cd40a74c1bab8664a31cfe6c97e271
web3online <placex.com@gmail.com>
2022-11-23 12:07:56 +0200

    introduce new constant ARCHIVE_NODE_LEDGER_BATCH

--- clients/vault/src/oracle/constants.rs
+++ clients/vault/src/oracle/constants.rs
@@ -22,6 +22,8 @@ pub const VALIDATOR_COUNT_PUBLIC_NETWORK: usize = 23;
 
 pub const MAX_SLOT_TO_REMEMBER: u64 = 12;
 
+pub const ARCHIVE_NODE_LEDGER_BATCH: i32 = 64;
+
 pub const stellar_history_base_url: &str =
 	"http://history.stellar.org/prd/core-live/core_live_002/scp/";
 
--- clients/vault/src/oracle/storage/impls.rs
+++ clients/vault/src/oracle/storage/impls.rs
@@ -11,7 +11,7 @@ use stellar_relay::sdk::{
 
 use crate::oracle::{
 	storage::traits::*, EnvelopesFileHandler, EnvelopesMap, Error, Filename, SerializedData, Slot,
-	SlotEncodedMap, TxHashMap, TxHashesFileHandler, TxSetMap, TxSetsFileHandler,
+	SlotEncodedMap, TxHashMap, TxHashesFileHandler, TxSetMap, TxSetsFileHandler, constants::ARCHIVE_NODE_LEDGER_BATCH,
 };
 
 use stellar_relay::sdk::XdrCodec;
@@ -212,11 +212,11 @@ impl ScpArchiveStorage {
 	}
 
 	fn find_last_slot_index_in_batch(slot_index: i32) -> i32 {
-		let rest = (slot_index + 1) % 64;
+		let rest = (slot_index + 1) % ARCHIVE_NODE_LEDGER_BATCH;
 		if rest == 0 {
 			return slot_index
 		}
-		return slot_index + 64 - rest
+		return slot_index + ARCHIVE_NODE_LEDGER_BATCH - rest
 	}
 
 	fn read_file_xdr(filename: &str) -> Result<Vec<u8>, Error> {
