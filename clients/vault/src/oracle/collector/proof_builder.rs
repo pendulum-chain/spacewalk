ced7cba0aa09fecc420e3b6f3b816d5804588bf1
b-yap <2826165+b-yap@users.noreply.github.com>
2022-12-08 21:20:02 +0800

    fix a bug: only those slots with corresponding issue ids SHOULD be in the pending list.

--- clients/vault/src/oracle/collector/proof_builder.rs
+++ clients/vault/src/oracle/collector/proof_builder.rs
@@ -17,16 +17,6 @@ use crate::oracle::{
 	ScpArchiveStorage, ScpMessageCollector, Slot,
 };
 
-#[async_trait::async_trait]
-pub trait ProofExt: Send + Sync {
-	async fn get_proof(&self, slot: Slot) -> Result<ProofStatus, crate::oracle::Error>;
-
-	/// Returns a list of transactions with each of their corresponding proofs
-	async fn get_pending_proofs(&self) -> Result<Vec<Proof>, crate::oracle::Error>;
-
-	async fn processed_proof(&self, slot: Slot);
-}
-
 /// The Proof of Transactions that needed to be processed
 #[derive(Debug, Eq, PartialEq)]
 pub struct Proof {
