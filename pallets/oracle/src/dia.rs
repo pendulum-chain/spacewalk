618c32499d4dcb4735d36822f19d4a0ff3410db1
web3online <placex.com@gmail.com>
2023-01-11 20:15:52 +0000

    add support DiaOracleAdapter conversion spacewalk oracle key <-> dia

--- pallets/oracle/src/dia.rs
+++ pallets/oracle/src/dia.rs
@@ -5,14 +5,36 @@ use sp_std::{
 };
 use dia_oracle::DiaOracle;
 
-pub struct DiaOracleAdapter<DiaPallet : DiaOracle, UnsignedFixedPoint, Moment>(marker::PhantomData<(DiaPallet, UnsignedFixedPoint, Moment)>);
+use sp_runtime::{
+	traits::{Convert},
+};
 
-impl<T: DiaOracle, UnsignedFixedPoint, Moment> DataProviderExtended<OracleKey, TimestampedValue<UnsignedFixedPoint, Moment>> for DiaOracleAdapter<T, UnsignedFixedPoint, Moment >{
+struct X;
+
+impl Convert<OracleKey, Option<(Vec<u8>,Vec<u8>)>> for X{
+    fn convert(a: OracleKey) -> Option<(Vec<u8>,Vec<u8>)> {
+        todo!()
+    }
+}
+
+impl Convert<(Vec<u8>,Vec<u8>), Option<OracleKey>> for X{
+    fn convert(a: (Vec<u8>,Vec<u8>)) -> Option<OracleKey> {
+        todo!()
+    }
+}
+
+pub struct DiaOracleAdapter<DiaPallet : DiaOracle, UnsignedFixedPoint, Moment, ConvertKey>(marker::PhantomData<(DiaPallet, UnsignedFixedPoint, Moment, ConvertKey)>);
+
+impl<T: DiaOracle, UnsignedFixedPoint, Moment, ConvertKey> DataProviderExtended<OracleKey, TimestampedValue<UnsignedFixedPoint, Moment>> for DiaOracleAdapter<T, UnsignedFixedPoint, Moment, ConvertKey>
+    where ConvertKey : Convert<OracleKey, Option<(Vec<u8>,Vec<u8>)>> + Convert<(Vec<u8>,Vec<u8>), Option<OracleKey>>
+{
     fn get_no_op(key: &OracleKey) -> Option<TimestampedValue<UnsignedFixedPoint, Moment>> {
+        let dia_key : Option<(Vec<u8>,Vec<u8>)> = ConvertKey::convert(key.clone());
         todo!()
     }
 
     fn get_all_values() -> Vec<(OracleKey, Option<TimestampedValue<UnsignedFixedPoint, Moment>>)> {
+        // let r = T::get_coin_info(blockchain, symbol);
         todo!()
     }
 }
