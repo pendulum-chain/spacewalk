83e7ba7a53f4f2484ef188d9a0a5b9f45c3d1691
Marcel Ebert <mail@marcel-ebert.de>
2023-01-26 18:58:49 +0100

    Move benchmark related helpers to extra file

--- testchain/runtime/src/lib.rs
+++ testchain/runtime/src/lib.rs
@@ -6,7 +6,6 @@
 #[macro_use]
 extern crate frame_benchmarking;
 
-use cfg_if::cfg_if;
 use codec::Encode;
 pub use dia_oracle::dia::*;
 use frame_support::{
@@ -17,7 +16,6 @@ use frame_support::{
 };
 pub use frame_system::Call as SystemCall;
 use orml_currencies::BasicCurrencyAdapter;
-use orml_oracle::{DataProvider, TimestampedValue};
 use orml_traits::{currency::MutationHooks, parameter_type_with_key};
 pub use pallet_balances::Call as BalancesCall;
 use pallet_grandpa::{
@@ -45,7 +43,7 @@ use currency::Amount;
 pub use issue::{Event as IssueEvent, IssueRequest};
 pub use module_oracle_rpc_runtime_api::BalanceWrapper;
 pub use nomination::Event as NominationEvent;
-use oracle::{dia::DiaOracleAdapter, OracleKey};
+use oracle::dia::DiaOracleAdapter;
 use primitives::DiaOracleKeyConvertor;
 pub use primitives::{
 	self, AccountId, Balance, BlockNumber, CurrencyId, ForeignCurrencyId, Hash, Moment, Nonce,
@@ -460,86 +458,8 @@ impl Convert<u64, Option<Moment>> for ConvertMoment {
 	}
 }
 
-cfg_if::cfg_if! {
-	if #[cfg(any(feature = "testing-utils", feature = "runtime-benchmarks"))] {
-		use sp_std::collections::btree_map::BTreeMap as HashMap;
-		use spin::mutex::Mutex;
-		use oracle::oracle_mock::{*};
-
-		lazy_static::lazy_static! {
-			static ref COINS: Mutex<HashMap<DataKey, Data>> = Mutex::new(HashMap::<DataKey, Data>::new());
-		}
-
-		pub struct MockDiaOracle;
-		impl DiaOracle for MockDiaOracle {
-			fn get_coin_info(
-				blockchain: Vec<u8>,
-				symbol: Vec<u8>,
-			) -> Result<dia_oracle::CoinInfo, sp_runtime::DispatchError> {
-				let key = (blockchain, symbol);
-				let data_key =
-					DataKey { blockchain: key.0.clone(), symbol: key.1.clone() };
-				let mut result: Option<Data> = None;
-				let map = COINS.lock();
-				let data_option = map.get(&data_key);
-				match data_option {
-					Some(data) => result = Some(data.clone()),
-					None => {},
-				};
-
-				let Some(result) = result else {
-					return Err(sp_runtime::DispatchError::Other(""));
-				};
-				let mut coin_info = CoinInfo::default();
-				coin_info.price = result.price;
-				coin_info.last_update_timestamp = result.timestamp;
-
-				Ok(coin_info)
-			}
-
-			//Spacewalk DiaOracleAdapter does not use get_value function. There is no need to implement
-			// this function.
-			fn get_value(
-				_blockchain: Vec<u8>,
-				_symbol: Vec<u8>,
-			) -> Result<dia_oracle::PriceInfo, sp_runtime::DispatchError> {
-				unimplemented!(
-					"DiaOracleAdapter implementation of DataProviderExtended does not use this function."
-				)
-			}
-		}
-
-		// Integration tests feed_value prices data directly to DIA oracle pallet. Need defatult
-		// implementation only for compile this runtime for integration tests inside 'runtime' package.
-		pub struct DataCollector;
-
-		//DataFeeder required to implement DataProvider trait but there no need to implement get function
-		impl DataProvider<OracleKey, TimestampedValue<UnsignedFixedPoint, Moment>> for DataCollector {
-			fn get(_key: &OracleKey) -> Option<TimestampedValue<UnsignedFixedPoint, Moment>> {
-				unimplemented!("Not required to implement DataProvider get function")
-			}
-		}
-
-		impl orml_oracle::DataFeeder<OracleKey, TimestampedValue<UnsignedFixedPoint, Moment>, AccountId>
-		for DataCollector
-		{
-			fn feed_value(
-				_who: AccountId,
-				key: OracleKey,
-				value: TimestampedValue<UnsignedFixedPoint, Moment>,
-			) -> sp_runtime::DispatchResult {
-				let key = MockOracleKeyConvertor::convert(key).unwrap();
-				let price = value.value.into_inner();
-
-				let data_key = DataKey { blockchain: key.0.clone(), symbol: key.1.clone() };
-				let data = Data { key: data_key.clone(), price: price, timestamp: value.timestamp };
-
-				COINS.lock().insert(data_key, data);
-				Ok(())
-			}
-		}
-	}
-}
+#[cfg(any(feature = "testing-utils", feature = "runtime-benchmarks"))]
+mod benchmark_utils;
 
 cfg_if::cfg_if! {
 	 if #[cfg(feature = "testing-utils")] {
@@ -553,12 +473,12 @@ cfg_if::cfg_if! {
 		>;
 	} else if #[cfg(feature = "runtime-benchmarks")] {
 		type DataProviderImpl = DiaOracleAdapter<
-			MockDiaOracle,
+			benchmark_utils::MockDiaOracle,
 			UnsignedFixedPoint,
 			Moment,
-			oracle::oracle_mock::MockOracleKeyConvertor,
-			oracle::oracle_mock::MockConvertPrice,
-			oracle::oracle_mock::MockConvertMoment,
+			benchmark_utils::MockOracleKeyConvertor,
+			benchmark_utils::MockConvertPrice,
+			benchmark_utils::MockConvertMoment,
 		>;
 	} else {
 		type DataProviderImpl = DiaOracleAdapter<
@@ -578,7 +498,7 @@ impl oracle::Config for Runtime {
 	type DataProvider = DataProviderImpl;
 
 	#[cfg(any(feature = "runtime-benchmarks", feature = "testing-utils"))]
-	type DataFeedProvider = DataCollector;
+	type DataFeedProvider = benchmark_utils::DataCollector;
 }
 
 impl issue::Config for Runtime {
