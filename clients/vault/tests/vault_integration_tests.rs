65ea78a92e638834a0670b89d5a32b9002b0589b
b-yap <2826165+b-yap@users.noreply.github.com>
2023-02-07 00:36:29 +0800

    cleanup and rebase

--- clients/vault/tests/vault_integration_tests.rs
+++ clients/vault/tests/vault_integration_tests.rs
@@ -15,7 +15,7 @@ use tokio::{sync::RwLock, time::sleep};
 
 use primitives::H256;
 use runtime::{
-	integration::*, types::*, FixedPointNumber, FixedU128, IssuePallet, OraclePallet, RedeemPallet,
+	integration::*, types::*, FixedPointNumber, FixedU128, IssuePallet, RedeemPallet,
 	ReplacePallet, ShutdownSender, SpacewalkParachain, SudoPallet, UtilFuncs, VaultRegistryPallet,
 };
 use stellar_relay_lib::sdk::{PublicKey, XdrCodec};
