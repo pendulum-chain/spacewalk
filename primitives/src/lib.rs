eca67b3979e91d59d548c847b4b944bcaa67872d
b-yap <2826165+b-yap@users.noreply.github.com>
2023-02-09 23:49:20 +0800

    TryFrom `CurrencyId` Implementations Contain Repeated Code

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -527,22 +527,12 @@ impl TryFrom<(&str, &str)> for CurrencyId {
 	type Error = &'static str;
 
 	fn try_from(value: (&str, &str)) -> Result<Self, Self::Error> {
-		let slice = value.0;
 		let issuer_encoded = value.1;
 		let issuer_pk = stellar::PublicKey::from_encoding(issuer_encoded)
 			.map_err(|_| "Invalid issuer encoding")?;
 		let issuer: AssetIssuer = *issuer_pk.as_binary();
-		if slice.len() <= 4 {
-			let mut code: Bytes4 = [0; 4];
-			code[..slice.len()].copy_from_slice(slice.as_bytes());
-			Ok(CurrencyId::AlphaNum4 { code, issuer })
-		} else if slice.len() > 4 && slice.len() <= 12 {
-			let mut code: Bytes12 = [0; 12];
-			code[..slice.len()].copy_from_slice(slice.as_bytes());
-			Ok(CurrencyId::AlphaNum12 { code, issuer })
-		} else {
-			Err("More than 12 bytes not supported")
-		}
+
+		CurrencyId::try_from((value.0, issuer))
 	}
 }
 
