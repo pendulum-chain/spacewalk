246b756225ad9bfa7372fbe3bc5e45bbc3259dae
b-yap <2826165+b-yap@users.noreply.github.com>
2023-02-13 19:55:01 +0800

    since DOT is already at 20, 20 should not fail. changed it to 30.

--- primitives/src/tests.rs
+++ primitives/src/tests.rs
@@ -180,7 +180,7 @@ fn test_addr_conversion() {
 fn test_foreign_currency_conversion_u64() {
 	assert_eq!(0u64.try_into(), Ok(ForeignCurrencyId::KSM));
 	assert_eq!(14u64.try_into(), Ok(ForeignCurrencyId::PHA));
-	assert_eq!(<u64 as TryInto<ForeignCurrencyId>>::try_into(20), Err(()));
+	assert_eq!(<u64 as TryInto<ForeignCurrencyId>>::try_into(30), Err(()));
 	assert_eq!(<u64 as TryInto<ForeignCurrencyId>>::try_into(257), Err(()));
 }
 
