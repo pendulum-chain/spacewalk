e6589244440af40fb1bb07a6f151a992144c7aae
Marcel Ebert <mail@marcel-ebert.de>
2023-02-16 18:07:08 +0000

    Automatically insert key for off-chain worker

--- testchain/node/src/service.rs
+++ testchain/node/src/service.rs
@@ -12,6 +12,7 @@ use sc_service::{
 };
 use sc_telemetry::{Telemetry, TelemetryWorker};
 use sp_consensus_aura::sr25519::AuthorityPair as AuraPair;
+use sp_core::crypto::KeyTypeId;
 
 use primitives::Block;
 use spacewalk_runtime::RuntimeApi;
@@ -223,6 +224,17 @@ pub fn new_full(mut config: Configuration) -> Result<(TaskManager, RpcHandlers),
 		})?;
 
 	if config.offchain_worker.enabled {
+		// Initialize seed for signing transaction using off-chain workers. This is a convenience
+		// so learners can see the transactions submitted simply running the node.
+		// Typically these keys should be inserted with RPC calls to `author_insertKey`.
+		let keystore = keystore_container.sync_keystore();
+		// Note that the `KeyTypeId` here is very important and the content must match the one
+		// defined in the DIA oracle pallet. Otherwise the signer key is not available for the
+		// off-chain worker of the pallet.
+		pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"dia!");
+		sp_keystore::SyncCryptoStore::sr25519_generate_new(&*keystore, KEY_TYPE, Some("//Bob"))
+			.expect("Creating key with account Alice should succeed.");
+
 		sc_service::build_offchain_workers(
 			&config,
 			task_manager.spawn_handle(),
