d384ca5e2a79e5bb4724bfbb4a3cff47eb1253f1
cr4pt0 <placex.com@gmail.com>
2023-03-24 16:24:20 +0000

    Revert "switch the function calls"
    This reverts commit 566dec321e70ae55fc99ab9dbf679997a833a288.

--- pallets/oracle/src/lib.rs
+++ pallets/oracle/src/lib.rs
@@ -269,9 +269,9 @@ impl<T: Config> Pallet<T> {
 		let converted = match (amount.currency(), currency_id) {
 			(x, y) if x == y => amount.amount(),
 			(_, _) => {
-				// first convert to USD, then convert USD to the desired currency
-				let base = Self::currency_to_usd(amount.amount(), amount.currency())?;
-				Self::usd_to_currency(base, currency_id)?
+				// first convert to usd, then convert usd to the desired currency
+				let base = Self::usd_to_currency(amount.amount(), amount.currency())?;
+				Self::currency_to_usd(base, currency_id)?
 			},
 		};
 		Ok(Amount::new(converted, currency_id))
