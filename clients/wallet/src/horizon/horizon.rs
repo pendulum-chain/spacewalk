e270702eaf71cfca99219b9749b2bfe35236c493
b-yap <2826165+b-yap@users.noreply.github.com>
2023-09-04 14:30:18 +0800

    https://github.com/pendulum-chain/spacewalk/pull/390#discussion_r1313055086, https://github.com/pendulum-chain/spacewalk/pull/390#discussion_r1313061019, https://github.com/pendulum-chain/spacewalk/pull/390#discussion_r1313067877, https://github.com/pendulum-chain/spacewalk/pull/390#discussion_r1313058275, https://github.com/pendulum-chain/spacewalk/pull/390#discussion_r1313056515, https://github.com/pendulum-chain/spacewalk/actions/runs/6048709746/job/16414585252?pr=390#step:12:38

--- clients/wallet/src/horizon/horizon.rs
+++ clients/wallet/src/horizon/horizon.rs
@@ -301,7 +301,6 @@ where
 
 	#[cfg(not(any(test, feature = "testing-utils")))]
 	let mut last_cursor = 0;
-	tracing::info!("CARLA CARLA CARLA listen_for_new_transactions");
 
 	#[cfg(any(test, feature = "testing-utils"))]
 	let mut last_cursor = last_cursor;
