ec59f64162c85d3a8233256d3445ccae11b53dad
b-yap <2826165+b-yap@users.noreply.github.com>
2023-09-05 18:06:34 +0800

    9B2-05 | Commented Out Code (partial)

--- clients/wallet/src/horizon/responses.rs
+++ clients/wallet/src/horizon/responses.rs
@@ -362,9 +362,7 @@ pub struct ClaimableBalance {
 #[derive(Deserialize, Encode, Decode, Default, Debug)]
 pub struct Claimant {
 	#[serde(deserialize_with = "de_string_to_bytes")]
-	pub destination: Vec<u8>,
-	// For now we assume that the predicate is always unconditional
-	// pub predicate: serde_json::Value,
+	pub destination: Vec<u8>
 }
 
 /// An iter structure equivalent to a list of TransactionResponse
