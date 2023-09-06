f5611ceba244117e25a5bb956bc62a8963fab443
b-yap <2826165+b-yap@users.noreply.github.com>
2023-09-06 14:57:58 +0800

    LBC-03 | Inconsistent `match` Expression

--- pallets/issue/src/lib.rs
+++ pallets/issue/src/lib.rs
@@ -467,50 +467,16 @@ impl<T: Config> Pallet<T> {
 		externalized_envelopes_encoded: Vec<u8>,
 		transaction_set_encoded: Vec<u8>,
 	) -> Result<(), DispatchError> {
-		let mut issue = Self::get_issue_request_from_id(&issue_id)?;
+		let mut issue = Self::get_executable_issue_request_from_id(&issue_id)?;
 		// allow anyone to complete issue request
 		let requester = issue.requester.clone();
 
-		let transaction_envelope = ext::stellar_relay::construct_from_raw_encoded_xdr::<
-			T,
-			TransactionEnvelope,
-		>(&transaction_envelope_xdr_encoded)?;
-
-		let envelopes = ext::stellar_relay::construct_from_raw_encoded_xdr::<
-			T,
-			UnlimitedVarArray<ScpEnvelope>,
-		>(&externalized_envelopes_encoded)?;
-
-		let transaction_set = ext::stellar_relay::construct_from_raw_encoded_xdr::<
-			T,
-			TransactionSet,
-		>(&transaction_set_encoded)?;
-
-		let shortened_request_id = derive_shortened_request_id(&issue_id.0);
-		// Check that the transaction includes the expected memo to mitigate replay attacks
-		ext::stellar_relay::ensure_transaction_memo_matches::<T>(
-			&transaction_envelope,
-			&shortened_request_id,
-		)?;
-
-		// Verify that the transaction is valid
-		ext::stellar_relay::validate_stellar_transaction::<T>(
-			&transaction_envelope,
-			&envelopes,
-			&transaction_set,
-		)
-		.map_err(|e| {
-			log::error!(
-				"failed to validate transaction of issue id: {} with transaction envelope: {transaction_envelope:?}",
-				hex::encode(issue_id.as_bytes())
-			);
-			e
-		})?;
-
-		let amount_transferred: Amount<T> = ext::currency::get_amount_from_transaction_envelope::<T>(
-			&transaction_envelope,
-			issue.stellar_address,
-			issue.asset,
+		let amount_transferred: Amount<T> = Self::validate_stellar_transaction(
+			&issue_id,
+			&issue,
+			&transaction_envelope_xdr_encoded,
+			&externalized_envelopes_encoded,
+			&transaction_set_encoded,
 		)?;
 
 		let expected_total_amount = issue.amount().checked_add(&issue.fee())?;
@@ -518,10 +484,6 @@ impl<T: Config> Pallet<T> {
 		match issue.status {
 			IssueRequestStatus::Completed => return Err(Error::<T>::IssueCompleted.into()),
 			IssueRequestStatus::Cancelled => {
-				// if vault is not accepting new issues, we don't allow the execution of cancelled
-				// issues, since this would drop the collateralization rate unexpectedly
-				ext::vault_registry::ensure_accepting_new_issues::<T>(&issue.vault)?;
-
 				// first try to increase the to-be-issued tokens - if the vault does not
 				// have sufficient collateral then this aborts
 				ext::vault_registry::try_increase_to_be_issued_tokens::<T>(
@@ -744,7 +706,7 @@ impl<T: Config> Pallet<T> {
 			.collect()
 	}
 
-	pub fn get_issue_request_from_id(
+	pub fn get_executable_issue_request_from_id(
 		issue_id: &H256,
 	) -> Result<DefaultIssueRequest<T>, DispatchError> {
 		let request = IssueRequests::<T>::try_get(issue_id).or(Err(Error::<T>::IssueIdNotFound))?;
@@ -752,7 +714,13 @@ impl<T: Config> Pallet<T> {
 		// NOTE: temporary workaround until we delete
 		match request.status {
 			IssueRequestStatus::Completed => Err(Error::<T>::IssueCompleted.into()),
-			_ => Ok(request),
+			IssueRequestStatus::Cancelled => {
+				// if vault is not accepting new issues, we don't allow the execution of cancelled
+				// issues, since this would drop the collateralization rate unexpectedly
+				ext::vault_registry::ensure_accepting_new_issues::<T>(&request.vault)?;
+				Ok(request)
+			},
+			IssueRequestStatus::Pending => Ok(request),
 		}
 	}
 
@@ -854,5 +822,57 @@ impl<T: Config> Pallet<T> {
 			);
 		}
 		Ok(())
+	}
+
+	fn validate_stellar_transaction(
+		issue_id: &H256,
+		issue: &DefaultIssueRequest<T>,
+		transaction_envelope_xdr_encoded: &[u8],
+		externalized_envelopes_encoded: &[u8],
+		transaction_set_encoded: &[u8],
+	) -> Result<Amount<T>, DispatchError> {
+		let transaction_envelope = ext::stellar_relay::construct_from_raw_encoded_xdr::<
+			T,
+			TransactionEnvelope,
+		>(&transaction_envelope_xdr_encoded)?;
+
+		let envelopes = ext::stellar_relay::construct_from_raw_encoded_xdr::<
+			T,
+			UnlimitedVarArray<ScpEnvelope>,
+		>(&externalized_envelopes_encoded)?;
+
+		let transaction_set = ext::stellar_relay::construct_from_raw_encoded_xdr::<
+			T,
+			TransactionSet,
+		>(&transaction_set_encoded)?;
+
+		let shortened_request_id = derive_shortened_request_id(&issue_id.0);
+		// Check that the transaction includes the expected memo to mitigate replay attacks
+		ext::stellar_relay::ensure_transaction_memo_matches::<T>(
+			&transaction_envelope,
+			&shortened_request_id,
+		)?;
+
+		// Verify that the transaction is valid
+		ext::stellar_relay::validate_stellar_transaction::<T>(
+			&transaction_envelope,
+			&envelopes,
+			&transaction_set,
+		)
+			.map_err(|e| {
+				log::error!(
+                "failed to validate transaction of issue id: {} with transaction envelope: {transaction_envelope:?}",
+                hex::encode(issue_id.as_bytes())
+            );
+				e
+			})?;
+
+		let amount_transferred: Amount<T> = ext::currency::get_amount_from_transaction_envelope::<T>(
+			&transaction_envelope,
+			issue.stellar_address,
+			issue.asset,
+		)?;
+
+		Ok(amount_transferred)
 	}
 }
