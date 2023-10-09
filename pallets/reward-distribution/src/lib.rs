3ffb25f0f9a9ea05da7766e7b0c7b237272f5e9b
Marcel Ebert <mail@marcel-ebert.de>
2023-10-09 18:27:58 +0200

    Finish base logic

--- pallets/reward-distribution/src/lib.rs
+++ pallets/reward-distribution/src/lib.rs
@@ -12,7 +12,13 @@ mod default_weights;
 
 pub use default_weights::{SubstrateWeight, WeightInfo};
 
-use frame_support::{dispatch::DispatchResult, traits::Get, transactional, BoundedVec};
+use crate::types::{AccountIdOf, BalanceOf};
+use frame_support::{
+	dispatch::DispatchResult,
+	traits::{Currency, Get},
+	transactional, BoundedVec,
+};
+use sp_arithmetic::Perquintill;
 
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
@@ -20,16 +26,7 @@ mod benchmarking;
 #[cfg(test)]
 mod mock;
 
-type NameOf<T> = BoundedVec<u8, <T as pallet::Config>::MaxNameLength>;
-type UriOf<T> = BoundedVec<u8, <T as pallet::Config>::MaxUriLength>;
-
-#[derive(Encode, Decode, Eq, PartialEq, Clone, Default, TypeInfo, Debug, MaxEncodedLen)]
-pub struct ClientRelease<Uri, Hash> {
-	/// URI to the client release binary.
-	pub uri: Uri,
-	/// The SHA256 checksum of the client binary.
-	pub checksum: Hash,
-}
+mod types;
 
 pub use pallet::*;
 
@@ -44,7 +41,7 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 	#[pallet::config]
-	pub trait Config: frame_system::Config {
+	pub trait Config: frame_system::Config + security::Config {
 		/// The overarching event type.
 		type RuntimeEvent: From<Event<Self>>
 			+ Into<<Self as frame_system::Config>::RuntimeEvent>
@@ -53,103 +50,54 @@ pub mod pallet {
 		/// Weight information for the extrinsics in this module.
 		type WeightInfo: WeightInfo;
 
-		/// The maximum length of a client name.
+		/// The currency trait.
+		type Currency: Currency<AccountIdOf<Self>>;
+
+		/// Defines the interval (in number of blocks) at which the reward per block decays.
 		#[pallet::constant]
-		type MaxNameLength: Get<u32>;
+		type DecayInterval: Get<Self::BlockNumber>;
 
-		/// The maximum length of a client URI.
+		/// Defines the rate at which the reward per block decays.
 		#[pallet::constant]
-		type MaxUriLength: Get<u32>;
-	}
-
-	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
-		fn on_runtime_upgrade() -> frame_support::weights::Weight {
-			crate::upgrade_client_releases::try_upgrade_current_client_releases::<T>()
-		}
-	}
-
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		/// Sets the current client release version, in case of a bug fix or patch.
-		///
-		/// # Arguments
-		/// * `client_name` - raw byte string representation of the client name (e.g. `b"vault"`)
-		/// * `release` - The release information for the given `client_name`
-		#[pallet::call_index(0)]
-		#[pallet::weight(<T as Config>::WeightInfo::set_current_client_release(T::MaxNameLength::get(), T::MaxUriLength::get()))]
-		#[transactional]
-		pub fn set_current_client_release(
-			origin: OriginFor<T>,
-			client_name: NameOf<T>,
-			release: ClientRelease<UriOf<T>, T::Hash>,
-		) -> DispatchResult {
-			ensure_root(origin)?;
-			CurrentClientReleases::<T>::insert(client_name, release.clone());
-			Self::deposit_event(Event::<T>::ApplyClientRelease { release });
-			Ok(())
-		}
-
-		/// Sets the pending client release version. To be batched alongside the
-		/// `parachainSystem.authorizeUpgrade` Cumulus call.
-		/// Clients include the vault, oracle, and faucet.
-		///
-		/// # Arguments
-		/// * `client_name` - raw byte string representation of the client name (e.g. `b"vault"`)
-		/// * `release` - The release information for the given `client_name`
-		#[pallet::call_index(1)]
-		#[pallet::weight(<T as Config>::WeightInfo::set_pending_client_release(T::MaxNameLength::get(), T::MaxUriLength::get()))]
-		#[transactional]
-		pub fn set_pending_client_release(
-			origin: OriginFor<T>,
-			client_name: NameOf<T>,
-			release: ClientRelease<UriOf<T>, T::Hash>,
-		) -> DispatchResult {
-			ensure_root(origin)?;
-			PendingClientReleases::<T>::insert(client_name, release.clone());
-			Self::deposit_event(Event::<T>::NotifyClientRelease { release });
-			Ok(())
-		}
+		type DecayRate: Get<Perquintill>;
 	}
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		NotifyClientRelease { release: ClientRelease<UriOf<T>, T::Hash> },
-		ApplyClientRelease { release: ClientRelease<UriOf<T>, T::Hash> },
+		/// A new RewardPerBlock value has been set.
+		RewardsPerBlockAdapted(BalanceOf<T>),
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {}
 
-	/// Mapping of client name (string literal represented as bytes) to its release details.
+	/// Reward per block.
 	#[pallet::storage]
-	#[pallet::getter(fn current_client_release)]
-	pub(super) type CurrentClientReleases<T: Config> =
-		StorageMap<_, Blake2_128Concat, NameOf<T>, ClientRelease<UriOf<T>, T::Hash>, OptionQuery>;
+	#[pallet::getter(fn reward_per_block)]
+	pub type RewardPerBlock<T: Config> = StorageValue<_, BalanceOf<T>, OptionQuery>;
 
-	/// Mapping of client name (string literal represented as bytes) to its pending release details.
 	#[pallet::storage]
-	#[pallet::getter(fn pending_client_release)]
-	pub(super) type PendingClientReleases<T: Config> =
-		StorageMap<_, Blake2_128Concat, NameOf<T>, ClientRelease<UriOf<T>, T::Hash>, OptionQuery>;
-}
+	#[pallet::getter(fn rewards_adapted_at)]
+	pub(super) type RewardsAdaptedAt<T: Config> = StorageValue<_, BlockNumberFor<T>, OptionQuery>;
 
-pub mod upgrade_client_releases {
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// Sets the reward per block.
+		#[pallet::call_index(0)]
+		#[pallet::weight(<T as Config>::WeightInfo::set_rewards_per_block())]
+		#[transactional]
+		pub fn set_rewards_per_block(
+			origin: OriginFor<T>,
+			new_rewards_per_block: BalanceOf<T>,
+		) -> DispatchResult {
+			ensure_root(origin)?;
 
-	use crate::*;
-	use frame_support::weights::Weight;
+			RewardPerBlock::<T>::put(new_rewards_per_block);
+			RewardsAdaptedAt::<T>::put(frame_system::Pallet::<T>::block_number());
 
-	/// For each pending client release, set the current release to that.
-	/// The pending release entry is removed.
-	pub fn try_upgrade_current_client_releases<T: Config>() -> Weight {
-		let mut reads = 0;
-		for (key, release) in PendingClientReleases::<T>::drain() {
-			//log::info!("Upgrading client release for key {:?}", key);
-			CurrentClientReleases::<T>::insert(key, release.clone());
-			Pallet::<T>::deposit_event(Event::<T>::ApplyClientRelease { release });
-			reads += 1;
+			Self::deposit_event(Event::<T>::RewardsPerBlockAdapted(new_rewards_per_block));
+			Ok(())
 		}
-		T::DbWeight::get().reads_writes(reads, reads * 2)
 	}
 }
---
+++ pallets/reward-distribution/src/types.rs
@@ -0,0 +1,5 @@
+use crate::Config;
+
+pub type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+
+pub(crate) type BalanceOf<T> = <T as Config>::Balance;
