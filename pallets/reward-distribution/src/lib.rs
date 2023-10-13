496197f168e45b00e39911b28b3489906ddad6d1
Gianfranco Tasteri <gianfra33@gmail.com>
2023-10-13 09:50:08 -0300

    WIP base implementation

--- pallets/reward-distribution/src/lib.rs
+++ pallets/reward-distribution/src/lib.rs
@@ -7,10 +7,9 @@
 
 mod default_weights;
 
-pub use default_weights::{SubstrateWeight, WeightInfo};
-
 use crate::types::{AccountIdOf, BalanceOf, DefaultVaultId};
-use codec::{FullCodec, MaxEncodedLen};
+use codec::{FullCodec, FullEncode, MaxEncodedLen};
+pub use default_weights::{SubstrateWeight, WeightInfo};
 use frame_support::{
 	dispatch::DispatchResult,
 	pallet_prelude::DispatchError,
@@ -18,10 +17,10 @@ use frame_support::{
 	transactional,
 };
 use pooled_rewards::RewardsApi;
-use primitives::CurrencyId;
 use sp_arithmetic::Perquintill;
+use sp_core::bounded_vec::BoundedVec;
 use sp_runtime::{
-	traits::{AtLeast32BitUnsigned, CheckedAdd, One},
+	traits::{AtLeast32BitUnsigned, CheckedAdd, One, Zero},
 	FixedPointOperand,
 };
 use sp_std::fmt::Debug;
@@ -60,7 +59,13 @@ pub mod pallet {
 		type WeightInfo: WeightInfo;
 
 		/// The currency trait.
-		type Currency: Currency<AccountIdOf<Self>> + Clone;
+		type Currency: Currency<AccountIdOf<Self>>
+			+ Clone
+			+ Decode
+			+ FullEncode
+			+ TypeInfo
+			+ MaxEncodedLen
+			+ Debug;
 
 		//balance
 		type Balance: AtLeast32BitUnsigned
@@ -86,6 +91,8 @@ pub mod pallet {
 
 		type OracleApi: ToUsdApi<Self::Balance, Self::Currency>;
 
+		type NativeToken: Get<Self::Currency>;
+
 		/// Defines the interval (in number of blocks) at which the reward per block decays.
 		#[pallet::constant]
 		type DecayInterval: Get<Self::BlockNumber>;
@@ -103,12 +110,15 @@ pub mod pallet {
 	}
 
 	#[pallet::error]
-	pub enum Error<T> {}
+	pub enum Error<T> {
+		//Overflow
+		Overflow,
+	}
 
 	#[pallet::hooks]
 	impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {
 		fn on_initialize(n: T::BlockNumber) -> Weight {
-			Self::distribute_rewards(n);
+			Self::execute_on_init(n);
 			//TODO benchmark this weight properly
 			<T as Config>::WeightInfo::on_initialize()
 		}
@@ -127,7 +137,10 @@ pub mod pallet {
 	#[pallet::getter(fn rewards_percentage)]
 	pub(super) type RewardsPercentage<T: Config> = StorageValue<
 		_,
-		BoundedVec<(CurrencyId, Perquintill), <T as pallet::Config>::MaxCurrencies>,
+		BoundedVec<
+			(<T as pallet::Config>::Currency, Perquintill),
+			<T as pallet::Config>::MaxCurrencies,
+		>,
 		OptionQuery,
 	>;
 
@@ -153,7 +166,7 @@ pub mod pallet {
 }
 
 impl<T: Config> Pallet<T> {
-	pub fn distribute_rewards(height: T::BlockNumber) {
+	pub fn execute_on_init(height: T::BlockNumber) {
 		//get reward per block
 		let reward_per_block = RewardPerBlock::<T>::get();
 		if reward_per_block == None {
@@ -168,69 +181,69 @@ impl<T: Config> Pallet<T> {
 		//update the reward per block if decay interval passed
 		let rewards_adapted_at = RewardsAdaptedAt::<T>::get();
 		if ext::security::parachain_block_expired::<T>(
-			rewards_adapted_at.expect("checked for none"),
+			rewards_adapted_at.expect("HANDLE"),
 			T::DecayInterval::get() - T::BlockNumber::one(),
 		)
 		.unwrap()
 		{
 			let decay_rate = T::DecayRate::get();
-			let reward_this_block =
-				decay_rate.mul_floor(reward_per_block.expect("checked for none"));
-			RewardPerBlock::<T>::set(Some(new_reward_per_block));
+			reward_this_block = decay_rate.mul_floor(reward_per_block.expect("checked for none"));
+			RewardPerBlock::<T>::set(Some(reward_this_block));
 
 			RewardsAdaptedAt::<T>::set(Some(height));
 		}
 
-		let total_reward_stake = Self::caculate_reward_stake_percentages(reward_this_block).unwrap();
-
-	
+		//TODO how to handle error if on init cannot fail?
+		let _ = Self::distribute_rewards(T::NativeToken::get(), reward_this_block);
 	}
 
 	//fetch total stake (all), and calulate total usd stake in percentage across pools
-	fn caculate_reward_stake_percentages() -> Result<(), ()> {
+	fn distribute_rewards(
+		reward_currency: T::Currency,
+		reward_amount: BalanceOf<T>,
+	) -> Result<(), DispatchError> {
 		let total_stakes = T::VaultRewards::get_total_stake_all_pools().unwrap();
 		let total_stake_in_usd = BalanceOf::<T>::default();
 		for (currency_id, stake) in total_stakes.clone().into_iter() {
-			let stake_in_usd = T::OracleApi::currency_to_usd(stake, currency_id).unwrap();
+			let stake_in_usd = T::OracleApi::currency_to_usd(&stake, &currency_id).unwrap();
 			total_stake_in_usd.checked_add(&stake_in_usd).unwrap();
 		}
-		Ok(total_stake_in_usd)
 
-		let percentages_vec: Vec::new();
+		let mut percentages_vec = Vec::<(T::Currency, Perquintill)>::new();
 		for (currency_id, stake) in total_stakes.into_iter() {
-			let stake_in_amount = Amount::<T>::new(stake, currency_id);
-			let stake_in_usd = stake_in_amount.convert_to(<T as Config>::BaseCurrency::get())?;
-			let percentage = Perquintill::from_rational(stake_in_usd.amount(), total_stake_in_usd);
+			let stake_in_usd = T::OracleApi::currency_to_usd(&stake, &currency_id)?;
+			let percentage = Perquintill::from_rational(stake_in_usd, total_stake_in_usd);
+
+			let reward_for_pool = percentage.mul_floor(reward_amount);
+			let error_reward_accum = BalanceOf::<T>::zero();
+			if T::VaultRewards::distribute_reward(
+				&currency_id,
+				reward_currency.clone(),
+				reward_for_pool,
+			)
+			.is_err()
+			{
+				error_reward_accum.checked_add(&reward_for_pool).ok_or(Error::<T>::Overflow)?;
+			}
 
 			percentages_vec.push((currency_id, percentage))
 		}
 
-		let bounded_vec = BoundedVec::try_from(percentages_vec).expect("should not be longer than max currencies");
+		//we store the calculated percentages which are good as long as
+		//prices are unchanged
+		let bounded_vec = BoundedVec::try_from(percentages_vec)
+			.expect("should not be longer than max currencies");
+		RewardsPercentage::<T>::set(Some(bounded_vec));
 
-		RewardsPercentage::<T>::set(bounded_vec);
+		Ok(())
 	}
-
-	pub fn distribute_rewards(reward)-> Result<(),(){
-		// multiply with floor or ceil?
-
-		let stake_percentages = RewardsPercentage::<T>::get();
-		
-		for (currency_id, stake) in stake_percentages.into_iter() {
-			let reward_for_pool = percentage.mul_floor(reward.amount());
-			let error_reward_accum = Amount::<T>::zero(reward.currency());
-			if T::VaultRewards::distribute_reward(&currency_id, reward.currency(), reward_for_pool)
-				.is_err()
-			{
-				error_reward_accum.checked_add(&reward)?;
-			}
-		}
-		
-	}
-
 }
 
-
-
+//TODO I actually want this api coming form oracle (defined and implemented)
+//but importing oracle gives an unexpected and unrelated error
 pub trait ToUsdApi<Balance, CurrencyId> {
-	fn currency_to_usd(amount: Balance, currency_id: CurrencyId) -> Result<Balance, DispatchError>;
+	fn currency_to_usd(
+		amount: &Balance,
+		currency_id: &CurrencyId,
+	) -> Result<Balance, DispatchError>;
 }
