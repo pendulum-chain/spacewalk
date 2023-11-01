834e1d62636266e6516ccb8d0d162bf2eff8fbb5
Marcel Ebert <mail@marcel-ebert.de>
2023-11-01 15:09:45 +0100

    Fix Github `build` action and improve releases (#422)
    * Add new workflow that triggers the CI in Gitlab
    
    * Fix parameters in other workflow file
    
    * Change build.yml
    
    * Amend comment
    
    * Create script to automatically bump version
    
    * Create script to automatically bump to new minor version
    
    * Update README.md
    
    * Remove `debug` flag from build.yml

---
+++ .maintain/create_patch_release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Find all Cargo.toml files in the current directory and its subdirectories
+for file in $(find . -name "Cargo.toml")
+do
+    # Use awk to increment the version number of the package
+    awk -F'.' '/\[package\]/,/version =/ { if($0 ~ /version =/ && $0 !~ /#/) {print $1"."$2"."$3+1; next} }1' $file > temp && mv temp $file
+done
