ee24e88f51cbaf1693536ad82a877fa771f8c915
b-yap <2826165+b-yap@users.noreply.github.com>
2023-12-01 12:59:00 +0800

    cleanup

--- clients/stellar-relay-lib/src/connection/connector/connector.rs
+++ clients/stellar-relay-lib/src/connection/connector/connector.rs
@@ -5,7 +5,7 @@ use substrate_stellar_sdk::{
 };
 use substrate_stellar_sdk::types::StellarMessage;
 use tokio::io::AsyncWriteExt;
-use tokio::net::tcp::OwnedWriteHalf;
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 use tokio::sync::mpsc;
 use tokio::sync::mpsc::Sender;
 
@@ -19,6 +19,8 @@ use crate::{
 	},
 	node::{LocalInfo, NodeInfo, RemoteInfo},
 };
+use crate::connection::connector::message_reader::read_message_from_stellar;
+use crate::helper::create_stream;
 
 pub struct Connector {
 	local: LocalInfo,
@@ -210,4 +212,72 @@ impl Connector {
 	) {
 		self.flow_controller.enable(local_overlay_version, remote_overlay_version)
 	}
+}
+
+
+/// Polls for messages coming from the Stellar Node and communicates it back to the user
+///
+/// # Arguments
+/// * `connector` - contains the config and necessary info for connecting to Stellar Node
+/// * `r_stream` - the read half of the stream that is connected to Stellar Node
+/// * `send_to_user_sender` - sends message from Stellar to the user
+/// * `send_to_node_receiver` - receives message from user and writes it to the write half of the stream.
+pub(crate) async fn poll_messages_from_stellar(
+	mut connector: Connector,
+	mut r_stream: OwnedReadHalf,
+	address: String,
+	send_to_user_sender: mpsc::Sender<StellarMessage>,
+	mut send_to_node_receiver: mpsc::Receiver<StellarMessage>
+) {
+	log::info!("poll_messages_from_stellar(): started.");
+
+	loop {
+		if send_to_user_sender.is_closed() {
+			log::info!("poll_messages_from_stellar(): closing receiver during disconnection");
+			// close this channel as communication to user was closed.
+			break;
+		}
+
+		tokio::select! {
+			result_msg = read_message_from_stellar(&mut r_stream, connector.timeout_in_secs) => match result_msg {
+                Err(e) => {
+                    log::error!("poll_messages_from_stellar(): {e:?}");
+                    break;
+                },
+				Ok(xdr) =>  match connector.process_raw_message(xdr).await {
+                    Ok(Some(stellar_msg)) =>
+                    // push message to user
+                    if let Err(e) = send_to_user_sender.send(stellar_msg).await {
+                        log::warn!("poll_messages_from_stellar(): Error occurred during sending message to user: {e:?}");
+                    },
+                    Ok(_) => log::info!("poll_messages_from_stellar(): no message for user"),
+                    Err(e) => {
+                        log::error!("poll_messages_from_stellar(): Error occurred during processing xdr message: {e:?}");
+                        break;
+                    }
+                }
+			},
+
+			// push message to Stellar Node
+			Some(msg) = send_to_node_receiver.recv() => if let Err(e) = connector.send_to_node(msg).await {
+                log::error!("poll_messages_from_stellar(): Error occurred during sending message to node: {e:?}");
+            }
+		}
+	}
+	// make sure to drop/shutdown the stream
+	connector.wr.forget();
+	drop(r_stream);
+
+	send_to_node_receiver.close();
+	drop(send_to_user_sender);
+
+	log::info!("poll_messages_from_stellar(): stopped.");
+}
+
+async fn restart(connector: &mut Connector, address: &str) -> Result<OwnedReadHalf,Error> {
+	// split the stream for easy handling of read and write
+	let (rd, wr) = create_stream(address).await?;
+
+	connector.wr = wr;
+	Ok(rd)
 }
---
+++ clients/stellar-relay-lib/src/overlay.rs
@@ -0,0 +1,96 @@
+use substrate_stellar_sdk::types::{ErrorCode, StellarMessage};
+use tokio::net::{tcp, TcpStream};
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::error::{SendError, TryRecvError};
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::sync::oneshot;
+
+use crate::connection::{ConnectionInfo, Connector, poll_messages_from_stellar};
+use crate::Error;
+use crate::helper::{create_stream, error_to_string};
+use crate::node::NodeInfo;
+
+
+/// Used to send/receive messages to/from Stellar Node
+pub struct StellarOverlayConnection {
+    sender: mpsc::Sender<StellarMessage>,
+    receiver: mpsc::Receiver<StellarMessage>
+}
+
+impl StellarOverlayConnection {
+    pub async fn send_to_node(&self, msg:StellarMessage) -> Result<(), SendError<StellarMessage>> {
+        self.sender.send(msg).await
+    }
+
+    pub async fn listen(&mut self) -> Result<Option<StellarMessage>, Error> {
+        loop {
+            match self.receiver.try_recv() {
+                Ok(StellarMessage::ErrorMsg(e)) => {
+                    log::error!("listen(): received error message: {e:?}");
+                    if e.code == ErrorCode::ErrConf || e.code == ErrorCode::ErrAuth {
+                        return Err(Error::ConnectionFailed(error_to_string(e)))
+                    }
+
+                    return Ok(None)
+                },
+                Ok(msg) => return Ok(Some(msg)),
+                Err(TryRecvError::Disconnected) => return Err(Error::Disconnected),
+                Err(TryRecvError::Empty) => continue,
+            }
+        }
+    }
+
+    pub fn is_alive(&self) -> bool {
+        let result = self.sender.is_closed();
+
+        if result {
+            drop(self);
+        }
+
+        !result
+    }
+
+    pub fn disconnect(&mut self) {
+        log::info!("disconnect(): closing channel");
+        self.receiver.close();
+    }
+}
+
+impl Drop for StellarOverlayConnection {
+    fn drop(&mut self) {
+        self.disconnect();
+    }
+}
+
+impl StellarOverlayConnection {
+    /// Returns an `StellarOverlayConnection` when a connection to Stellar Node is successful.
+    pub async fn connect(local_node_info: NodeInfo, conn_info: ConnectionInfo) -> Result<Self,Error> {
+        log::info!("connect(): connecting to {conn_info:?}");
+
+
+        // this is a channel to communicate with the user/caller.
+        let (send_to_user_sender, send_to_user_receiver) =
+            mpsc::channel::<StellarMessage>(1024);
+
+        let (send_to_node_sender, send_to_node_receiver) =
+            mpsc::channel::<StellarMessage>(1024);
+
+        // split the stream for easy handling of read and write
+        let (rd, wr) = create_stream(&conn_info.address()).await?;
+
+        let address = conn_info.address();
+        let mut connector = Connector::new(
+            local_node_info,
+            conn_info,
+            wr,
+        );
+        connector.send_hello_message().await?;
+
+        tokio::spawn(poll_messages_from_stellar(connector,rd,address, send_to_user_sender,send_to_node_receiver));
+
+        Ok(StellarOverlayConnection {
+            sender: send_to_node_sender,
+            receiver: send_to_user_receiver
+        })
+    }
+}
