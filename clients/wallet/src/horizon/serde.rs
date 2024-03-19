bf6d748501a0c14457de981255ef9a70d59219db
Marcel Ebert <mail@marcel-ebert.de>
2024-03-19 13:18:50 +0100

    Improve robustness of Spacewalk integration tests (#495)
    * Fix compilation of vault integration tests
    It was working with `--all --all-features` but not in isolation
    
    * fix issue:
     failed with HorizonResponseError(reqwest::Error { kind: Decode, source: Error("invalid type: string \"ySHX => SHX/XLM DAILY\", expected a borrowed string", line: 9484, column: 44) })
    
    * apply cargo fmt
    
    * apply cargo fmt
    
    * fix fetching of transactions
    
    * add is_empty for arrays and vecs
    
    * update test case
    
    * apply cargo fmt
    
    * extend test timeout
    
    * no more skipping of slots
    
    * String instead of Cow
    
    * https://github.com/pendulum-chain/spacewalk/pull/495/files#r1521774132
    
    * https://github.com/pendulum-chain/spacewalk/pull/495/files#r1521774132
    
    * reduce amount transfers to not deplete the accounts too fast;
    extend timeout to 20 minutes for test_issue_execution_succeeds_from_archive
    
    * revert back the slot to 1 ahead
    
    * rustfmt fix
    
    * introducing semaphores.
    https://users.rust-lang.org/t/reqwest-http-client-fails-when-too-much-concurrency/55644
    
    * Revert "introducing semaphores."
    
    This reverts commit d5a019e02ef8d79e43a80fe9c13a89e9e0ceccf5.
    
    * revert semaphores, retain ClientBuilder
    https://users.rust-lang.org/t/reqwest-http-client-fails-when-too-much-concurrency/55644/2
    
    * set duration
    
    * cargo fmt
    
    * https://github.com/pendulum-chain/spacewalk/pull/495#issuecomment-2006621518
    
    ---------
    
    Co-authored-by: b-yap <2826165+b-yap@users.noreply.github.com>

--- clients/wallet/src/horizon/serde.rs
+++ clients/wallet/src/horizon/serde.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Deserializer};
 use std::str::FromStr;
 
-pub fn de_string_to_bytes<'de, D>(de: D) -> Result<Vec<u8>, D::Error>
+pub fn de_str_to_bytes<'de, D>(de: D) -> Result<Vec<u8>, D::Error>
 where
 	D: Deserializer<'de>,
 {
@@ -9,7 +9,7 @@ where
 	Ok(s.as_bytes().to_vec())
 }
 
-pub fn de_string_to_u128<'de, D>(de: D) -> Result<u128, D::Error>
+pub fn de_str_to_u128<'de, D>(de: D) -> Result<u128, D::Error>
 where
 	D: Deserializer<'de>,
 {
@@ -17,7 +17,7 @@ where
 	u128::from_str(s).map_err(serde::de::Error::custom)
 }
 
-pub fn de_string_to_u64<'de, D>(de: D) -> Result<u64, D::Error>
+pub fn de_str_to_u64<'de, D>(de: D) -> Result<u64, D::Error>
 where
 	D: Deserializer<'de>,
 {
@@ -25,7 +25,7 @@ where
 	u64::from_str(s).map_err(serde::de::Error::custom)
 }
 
-pub fn de_string_to_i64<'de, D>(de: D) -> Result<i64, D::Error>
+pub fn de_str_to_i64<'de, D>(de: D) -> Result<i64, D::Error>
 where
 	D: Deserializer<'de>,
 {
@@ -33,7 +33,7 @@ where
 	i64::from_str(s).map_err(serde::de::Error::custom)
 }
 
-pub fn de_string_to_f64<'de, D>(de: D) -> Result<f64, D::Error>
+pub fn de_str_to_f64<'de, D>(de: D) -> Result<f64, D::Error>
 where
 	D: Deserializer<'de>,
 {
@@ -41,7 +41,7 @@ where
 	f64::from_str(s).map_err(serde::de::Error::custom)
 }
 
-pub fn de_string_to_u32<'de, D>(de: D) -> Result<u32, D::Error>
+pub fn de_str_to_u32<'de, D>(de: D) -> Result<u32, D::Error>
 where
 	D: Deserializer<'de>,
 {
@@ -49,9 +49,16 @@ where
 	u32::from_str(s).map_err(serde::de::Error::custom)
 }
 
-pub fn de_string_to_optional_bytes<'de, D>(de: D) -> Result<Option<Vec<u8>>, D::Error>
+pub fn de_str_to_optional_bytes<'de, D>(de: D) -> Result<Option<Vec<u8>>, D::Error>
 where
 	D: Deserializer<'de>,
 {
 	Option::<&str>::deserialize(de).map(|opt_wrapped| opt_wrapped.map(|x| x.as_bytes().to_vec()))
+}
+
+pub fn de_string_to_optional_bytes<'de, D>(de: D) -> Result<Option<Vec<u8>>, D::Error>
+where
+	D: Deserializer<'de>,
+{
+	Option::<String>::deserialize(de).map(|opt_wrapped| opt_wrapped.map(|x| x.as_bytes().to_vec()))
 }
